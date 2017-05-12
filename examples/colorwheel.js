ab6a96a991cd61bd2d8c87911dc94a75ddb96b17
Andrey Lushnikov <lushnikov@chromium.org>
2017-05-12 16:48:02 -0700

    Fix License header of certain examples
    Certain examples rely extensively on the originating PhantomJS
    examples.
    
    This patch fixes license headers of these examples to mention
    PhantomJS authors.

--- examples/colorwheel.js
+++ examples/colorwheel.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Google Inc. All rights reserved.
+ * Copyright 2017 Google Inc., PhantomJS Authors All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
