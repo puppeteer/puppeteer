c08f1447bb012e1487f6a3289df88233c931ed55
Andrey Lushnikov <lushnikov@chromium.org>
2017-05-14 23:26:37 -0700

    Slight code restructuring
    This patch:
    - moves phantom shim shell into a bin/ folder
    - introduces a new root index.js which exposes Browser to the
      dependent modules
    - adds forgotten LICENSE header to the install.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /**
  * Copyright 2017 Google Inc. All rights reserved.
  *
@@ -15,46 +14,6 @@
  * limitations under the License.
  */
 
-var vm = require('vm');
-var fs = require('fs');
-var path = require('path');
-var Browser = require('./lib/Browser');
-var argv = require('minimist')(process.argv.slice(2), {
-    alias: { v: 'version' },
-    boolean: ['headless'],
-    default: {'headless': true },
-});
-
-if (argv.version) {
-    console.log('Puppeteer v' + require('./package.json').version);
-    return;
-}
-
-if (argv['ssl-certificates-path']) {
-    console.error('Flag --ssl-certificates-path is not currently supported.\nMore information at https://github.com/aslushnikov/puppeteer/issues/1');
-    process.exit(1);
-    return;
-}
-
-var scriptArguments = argv._;
-if (!scriptArguments.length) {
-    console.log('puppeteer [scriptfile]');
-    return;
-}
-
-var scriptPath = path.resolve(process.cwd(), scriptArguments[0]);
-if (!fs.existsSync(scriptPath)) {
-    console.error(`script not found: ${scriptPath}`);
-    process.exit(1);
-    return;
-}
-
-var browser = new Browser({
-        remoteDebuggingPort: 9229,
-        headless: argv.headless,
-});
-
-var PhatomJs = require('./phantomjs');
-var context = PhatomJs.createContext(browser, scriptPath, argv);
-var scriptContent = fs.readFileSync(scriptPath, 'utf8');
-vm.runInContext(scriptContent, context);
+module.exports = {
+    Browser: require('./lib/Browser')
+};
