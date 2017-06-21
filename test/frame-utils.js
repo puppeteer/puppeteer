448ac4ce647b845c4ed4bb203c6599e9c0f8ba09
Andrey Lushnikov <lushnikov@chromium.org>
2017-06-21 07:45:13 -0700

    Reformat codebase into 2-spaces
    This patch:
    - reformats codebase to use 2-spaces instead of 4. This will
      align the project with other codebases (e.g. DevTools and Lighthouse)
    - enables eslint indentation checking
    
    References #19

--- test/frame-utils.js
+++ test/frame-utils.js
@@ -1,62 +1,62 @@
 var utils = module.exports = {
-    /**
-     * @param {!Page} page
-     * @param {string} frameId
-     * @param {string} url
-     * @return {!Promise}
-     */
-    attachFrame: async function(page, frameId, url) {
-        await page.evaluate(attachFrame, frameId, url);
+  /**
+   * @param {!Page} page
+   * @param {string} frameId
+   * @param {string} url
+   * @return {!Promise}
+   */
+  attachFrame: async function(page, frameId, url) {
+    await page.evaluate(attachFrame, frameId, url);
 
-        function attachFrame(frameId, url) {
-            var frame = document.createElement('iframe');
-            frame.src = url;
-            frame.id = frameId;
-            document.body.appendChild(frame);
-            return new Promise(x => frame.onload = x);
-        }
-    },
+    function attachFrame(frameId, url) {
+      var frame = document.createElement('iframe');
+      frame.src = url;
+      frame.id = frameId;
+      document.body.appendChild(frame);
+      return new Promise(x => frame.onload = x);
+    }
+  },
 
-    /**
-     * @param {!Page} page
-     * @param {string} frameId
-     * @return {!Promise}
-     */
-    detachFrame: async function(page, frameId) {
-        await page.evaluate(detachFrame, frameId);
+  /**
+   * @param {!Page} page
+   * @param {string} frameId
+   * @return {!Promise}
+   */
+  detachFrame: async function(page, frameId) {
+    await page.evaluate(detachFrame, frameId);
 
-        function detachFrame(frameId) {
-            var frame = document.getElementById(frameId);
-            frame.remove();
-        }
-    },
+    function detachFrame(frameId) {
+      var frame = document.getElementById(frameId);
+      frame.remove();
+    }
+  },
 
-    /**
-     * @param {!Page} page
-     * @param {string} frameId
-     * @param {string} url
-     * @return {!Promise}
-     */
-    navigateFrame: async function(page, frameId, url) {
-        await page.evaluate(navigateFrame, frameId, url);
+  /**
+   * @param {!Page} page
+   * @param {string} frameId
+   * @param {string} url
+   * @return {!Promise}
+   */
+  navigateFrame: async function(page, frameId, url) {
+    await page.evaluate(navigateFrame, frameId, url);
 
-        function navigateFrame(frameId, url) {
-            var frame = document.getElementById(frameId);
-            frame.src = url;
-            return new Promise(x => frame.onload = x);
-        }
-    },
+    function navigateFrame(frameId, url) {
+      var frame = document.getElementById(frameId);
+      frame.src = url;
+      return new Promise(x => frame.onload = x);
+    }
+  },
 
-    /**
-     * @param {!Frame} frame
-     * @param {string=} indentation
-     * @return {string}
-     */
-    dumpFrames: function(frame, indentation) {
-        indentation = indentation || '';
-        var result = indentation + frame.url();
-        for (var child of frame.childFrames())
-            result += '\n' + utils.dumpFrames(child, '    ' + indentation);
-        return result;
-    },
+  /**
+   * @param {!Frame} frame
+   * @param {string=} indentation
+   * @return {string}
+   */
+  dumpFrames: function(frame, indentation) {
+    indentation = indentation || '';
+    var result = indentation + frame.url();
+    for (var child of frame.childFrames())
+      result += '\n' + utils.dumpFrames(child, '    ' + indentation);
+    return result;
+  },
 };
