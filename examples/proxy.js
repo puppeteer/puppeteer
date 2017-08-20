43c3e533163d1cd7bfbddcb7b3a299fca0c3ef2c
Andrey Lushnikov <lushnikov@chromium.org>
2017-08-20 16:21:13 -0700

    Add proxy server example

---
+++ examples/proxy.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+const browser = await puppeteer.launch({
+  // Launch chromium using a proxy server on port 9876.
+  // More on proxing:
+  //    https://www.chromium.org/developers/design-documents/network-settings
+  args: [ '--proxy-server=127.0.0.1:9876' ]
+});
+const page = await browser.newPage();
+await page.goto('https://google.com');
+browser.close();
+
+})();
