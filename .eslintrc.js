c99513839e3afdcdb5e5c32c28206a9036ca6ec8
Joel <joel.einbinder@gmail.com>
2017-08-23 17:48:31 +0100

    Don't complain about Windows line endings on Windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -103,7 +103,7 @@ module.exports = {
         "no-multiple-empty-lines": [2, {"max": 2}],
         "no-mixed-spaces-and-tabs": 2,
         "no-trailing-spaces": 2,
-        "linebreak-style": [ 2, "unix" ],
+        "linebreak-style": [ process.platform === "win32" ? 0 : 2, "unix" ],
         "indent": [2, 2, { "SwitchCase": 1, "CallExpression": {"arguments": 2}, "MemberExpression": 2 }],
         "key-spacing": [2, {
             "beforeColon": false
