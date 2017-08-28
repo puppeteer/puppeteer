puppeteer/puppeteer

commit d5327e6a0fd384cd36bfd0b4a12b27ab362543a7

[api] Allow a custom userDataDir to be specified (#555)
This patch adds a `userDataDir` option to the `puppeteer.launch()` method.

Fixes #411.

lib/Launcher.js
@@ -53,11 +53,14 @@ class Launcher {
    */
   static async launch(options) {
     options = options || {};
-    const userDataDir = fs.mkdtempSync(CHROME_PROFILE_PATH);
+    let temporaryUserDataDir = null;
+    const chromeArguments = [].concat(DEFAULT_ARGS);
+    if (!options.args.some(arg => arg.startsWith('--user-data-dir'))) {
+      if (!options.userDataDir)
+        temporaryUserDataDir = fs.mkdtempSync(CHROME_PROFILE_PATH);
 
-    const chromeArguments = DEFAULT_ARGS.concat([
-      `--user-data-dir=${userDataDir}`,
-    ]);
+      chromeArguments.push(`--user-data-dir=${options.userDataDir || temporaryUserDataDir}`);
+    }
     if (typeof options.headless !== 'boolean' || options.headless) {
       chromeArguments.push(
           '--headless',
@@ -84,7 +87,8 @@ class Launcher {
     // Cleanup as processes exit.
     let killed = false;
     process.once('exit', killChrome);
-    chromeProcess.once('close', () => removeSync(userDataDir));
+    if (temporaryUserDataDir)
+      chromeProcess.once('close', () => removeSync(temporaryUserDataDir));
 
     if (options.handleSIGINT !== false)
       process.once('SIGINT', killChrome);

test/test.js
@@ -115,6 +115,25 @@ describe('Browser', function() {
     expect(await page.evaluate(() => 7 * 8)).toBe(56);
     originalBrowser.close();
   }));
+  it('userDataDir option', SX(async function() {
+    const userDataDir = fs.mkdtempSync(path.join(__dirname, 'test-user-data-dir'));
+    const options = Object.assign({userDataDir}, defaultBrowserOptions);
+    const browser = await puppeteer.launch(options);
+    expect(fs.readdirSync(userDataDir).length).toBeGreaterThan(0);
+    browser.close();
+    expect(fs.readdirSync(userDataDir).length).toBeGreaterThan(0);
+    rm(userDataDir);
+  }));
+  it('userDataDir argument', SX(async function() {
+    const userDataDir = fs.mkdtempSync(path.join(__dirname, 'test-user-data-dir'));
+    const options = Object.assign({}, defaultBrowserOptions);
+    options.args = [`--user-data-dir=${userDataDir}`].concat(options.args);
+    const browser = await puppeteer.launch(options);
+    expect(fs.readdirSync(userDataDir).length).toBeGreaterThan(0);
+    browser.close();
+    expect(fs.readdirSync(userDataDir).length).toBeGreaterThan(0);
+    rm(userDataDir);
+  }));
 });
 
 describe('Page', function() {

commit 8f66f7339e23c122e3abdcf1ff2075ed0dfbc76f

Fix launcher in case of empty arguments (#582)
The regression was introduced in
d5327e6a0fd384cd36bfd0b4a12b27ab362543a7

lib/Launcher.js
@@ -55,7 +55,7 @@ class Launcher {
     options = options || {};
     let temporaryUserDataDir = null;
     const chromeArguments = [].concat(DEFAULT_ARGS);
-    if (!options.args.some(arg => arg.startsWith('--user-data-dir'))) {
+    if (!options.args || !options.args.some(arg => arg.startsWith('--user-data-dir'))) {
       if (!options.userDataDir)
         temporaryUserDataDir = fs.mkdtempSync(CHROME_PROFILE_PATH);
 