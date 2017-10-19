2a5b1deed76355e7f06c5460a777566ad4122029
Andrey Lushnikov <lushnikov@chromium.org>
2017-10-19 11:20:03 -0700

    fix(page.evaluate): jsonValue should ignore toJSON property
    Currently, JSHandle.jsonValue() is implemented as in-page JSON.stringify
    call and consequent JSON.parse in node. This approach proved to be
    unfortunate for automation purposes: if page author overrode the
    Object.prototype.toJSON method, then it's harder for puppeteer to
    interact with the page.
    
    This patch switches JSHandle.jsonValue to use protocol serialization
    that ignores toJSON property. THis also changes the `page.evaluate`
    behavior since it is based on JSHandle.jsonValue().
    
    Fixes #1003.
    
    BREAKING CHANGE:
    `page.evaluate` no longer calls toJSON when generating return value.
    For the old behavior, do JSON.parse/JSON.stringify manually:
    
    ```js
    const json = JSON.parse(await page.evaluate(() => JSON.stringify(obj)));
    ```

--- test/test.js
+++ test/test.js
@@ -429,16 +429,16 @@ describe('Page', function() {
       const json = await aHandle.jsonValue();
       expect(json).toEqual({foo: 'bar'});
     }));
-    it('should work with dates', SX(async function() {
+    it('should not work with dates', SX(async function() {
       const dateHandle = await page.evaluateHandle(() => new Date('2017-09-26T00:00:00.000Z'));
       const json = await dateHandle.jsonValue();
-      expect(json).toBe('2017-09-26T00:00:00.000Z');
+      expect(json).toEqual({});
     }));
     it('should throw for circular objects', SX(async function() {
       const windowHandle = await page.evaluateHandle('window');
       let error = null;
       await windowHandle.jsonValue().catch(e => error = e);
-      expect(error.message).toContain('Converting circular structure to JSON');
+      expect(error.message).toContain('Object reference chain is too long');
     }));
   });
 
