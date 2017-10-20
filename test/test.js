0f64f3e9845443f91f17cbf2f378d3236734b8a6
Andrey Lushnikov <aslushnikov@gmail.com>
2017-10-20 10:45:22 -0700

    test: add test to make sure clipping is correct on retina displays (#1111)
    The test is currently disabled since it doesn't work.
    
    References #733

--- test/test.js
+++ test/test.js
@@ -2531,6 +2531,19 @@ describe('Page', function() {
       const screenshot = await page.screenshot({omitBackground: true});
       expect(screenshot).toBeGolden('transparent.png');
     }));
+    // Doesn't work on retina displays.
+    // @see https://github.com/GoogleChrome/puppeteer/issues/733
+    xit('should work with odd clip size on Retina displays', SX(async function() {
+      const screenshot = await page.screenshot({
+        clip: {
+          x: 0,
+          y: 0,
+          width: 11,
+          height: 11,
+        }
+      });
+      expect(screenshot).toBeGolden('screenshot-clip-odd-size.png');
+    }));
   });
 
   describe('Page.select', function() {
