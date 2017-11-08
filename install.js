8b41ce5b1002192d6b63a13bda5ed7a05d0dca62
Tomas Alabes <tomasAlabes@users.noreply.github.com>
2017-11-07 16:41:10 -0800

    support for npm no_proxy
    support npm no_proxy configuration for the installation script

--- install.js
+++ install.js
@@ -36,10 +36,14 @@ if (revisionInfo.downloaded)
 // Override current environment proxy settings with npm configuration, if any.
 const NPM_HTTPS_PROXY = process.env.npm_config_https_proxy || process.env.npm_config_proxy;
 const NPM_HTTP_PROXY = process.env.npm_config_http_proxy || process.env.npm_config_proxy;
+const NPM_NO_PROXY = process.env.npm_config_no_proxy;
+
 if (NPM_HTTPS_PROXY)
   process.env.HTTPS_PROXY = NPM_HTTPS_PROXY;
 if (NPM_HTTP_PROXY)
   process.env.HTTP_PROXY = NPM_HTTP_PROXY;
+if (NPM_NO_PROXY)
+  process.env.NO_PROXY = NPM_NO_PROXY;
 
 const allRevisions = Downloader.downloadedRevisions();
 const DOWNLOAD_HOST = process.env.PUPPETEER_DOWNLOAD_HOST || process.env.npm_config_puppeteer_download_host;
