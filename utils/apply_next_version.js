02a45dc4a18837c7a1aec53936a5ca4505ceab73
Joel Einbinder <einbinder@chromium.org>
2017-12-28 17:11:52 -0800

    set puppeteer@next version with the timestamp

--- utils/apply_next_version.js
+++ utils/apply_next_version.js
@@ -1,7 +1,12 @@
-let version = require('../package.json').version;
+const path = require('path');
+const fs = require('fs');
+
+const package = require('../package.json');
+let version = package.version;
 const dashIndex = version.indexOf('-');
 if (dashIndex !== -1)
   version = version.substring(0, dashIndex);
 version += '-next.' + Date.now();
-console.log("Setting version to " + version);
-require('child_process').execSync('npm version ' + version);
+console.log('Setting version to ' + version);
+package.version = version;
+fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(package, undefined, 2) + '\n');
