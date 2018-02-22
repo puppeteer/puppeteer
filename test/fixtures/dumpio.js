8d433e6312c82b3b19383c01ee40859b4baff6a8
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-02-22 10:55:30 +0200

    fix all comments

--- test/fixtures/dumpio.js
+++ test/fixtures/dumpio.js
@@ -1,9 +1,9 @@
 (async() => {
-  const [, , puppeteerRoot, options, emptyPage] = process.argv;
+  const [, , puppeteerRoot, options, emptyPage, dumpioTextToLog] = process.argv;
   const browser = await require(puppeteerRoot).launch(JSON.parse(options));
   const page = await browser.newPage();
   await page.goto(emptyPage);
-  await page.evaluate(() => console.log('test'));
+  await page.evaluate(_dumpioTextToLog => console.log(_dumpioTextToLog), dumpioTextToLog);
   await page.close();
   await browser.close();
 })();
