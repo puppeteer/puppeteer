c4b1ac99b0099ae6cb291fa11ff19e1eaeab1ff6
Andrey Lushnikov <aslushnikov@gmail.com>
2018-03-15 16:58:02 -0700

    test: another attempt to fix sw test #2201

--- test/test.js
+++ test/test.js
@@ -2855,7 +2855,7 @@ describe('Page', function() {
 
       // Load and re-load to make sure serviceworker is installed and running.
       await page.goto(server.PREFIX + '/serviceworkers/fetch/sw.html', {waitUntil: 'networkidle2'});
-      await page.evaluate(async() => await window.registrationPromise);
+      await page.evaluate(async() => await window.activationPromise);
       await page.reload();
 
       expect(responses.size).toBe(2);
@@ -3766,6 +3766,7 @@ describe('Page', function() {
     it('should report when a service worker is created and destroyed', async({page, server, browser}) => {
       await page.goto(server.EMPTY_PAGE);
       const createdTarget = new Promise(fulfill => browser.once('targetcreated', target => fulfill(target)));
+
       await page.goto(server.PREFIX + '/serviceworkers/empty/sw.html');
 
       expect((await createdTarget).type()).toBe('service_worker');
