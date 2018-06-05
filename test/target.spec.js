f43f378859dc3832f8ca8c55fa17ec7e642132f0
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-06-05 11:56:25 +0300

    add missing dependency after rebase

--- test/target.spec.js
+++ test/target.spec.js
@@ -16,7 +16,7 @@
 
 const {waitEvent} = require('./utils');
 
-module.exports.addTests = function({testRunner, expect, puppeteer}) {
+module.exports.addTests = function({testRunner, expect, puppeteer, browserWithExtensionOptions}) {
   const {describe, xdescribe, fdescribe} = testRunner;
   const {it, fit, xit} = testRunner;
   const {beforeAll, beforeEach, afterAll, afterEach} = testRunner;
