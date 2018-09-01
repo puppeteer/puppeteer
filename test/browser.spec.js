82a03ef7c7d27c80fbaa99f2a67bf6d793945206
Daniel Kidon <dandankidon@gmail.com>
2018-09-01 14:24:44 +0300

    added test coverage

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -48,4 +48,11 @@ module.exports.addTests = function({testRunner, expect, headless}) {
     });
   });
 
+  describe('Browser.chromeRunning', function() {
+    it('should return Boolean value', async function({browser}) {
+      const bool = await browser.chromeRunning();
+      expect(bool).toBeGreaterThan(0);
+    });
+  });
+
 };
