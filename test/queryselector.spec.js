6a18d1f73d3beebf9061656e78dcf8bef3abb84e
Andrey Lushnikov <lushnikov@chromium.org>
2018-11-20 18:40:37 -0800

    fix lint

--- test/queryselector.spec.js
+++ test/queryselector.spec.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const utils = require('./utils');
 
 module.exports.addTests = function({testRunner, expect, product}) {
   const {describe, xdescribe, fdescribe} = testRunner;
