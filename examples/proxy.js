4740885a4e0b9adc4a42834b47245cf462d37ae4
Andrey Lushnikov <lushnikov@chromium.org>
2019-01-09 17:11:11 -0800

    chore(examples): exclude localhost from proxy bypass list
    Since Chrome 72 localhost is bypassed by default. Oftentimes
    this is not a default behavior in testing scenarios.
    
    Fixes #3711.

--- examples/proxy.js
+++ examples/proxy.js
@@ -23,7 +23,11 @@ const puppeteer = require('puppeteer');
     // Launch chromium using a proxy server on port 9876.
     // More on proxying:
     //    https://www.chromium.org/developers/design-documents/network-settings
-    args: [ '--proxy-server=127.0.0.1:9876' ]
+    args: [
+      '--proxy-server=127.0.0.1:9876',
+      // Use proxy for localhost URLs
+      '--proxy-bypass-list=<-loopback>',
+    ]
   });
   const page = await browser.newPage();
   await page.goto('https://google.com');
