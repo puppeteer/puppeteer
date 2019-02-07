bd4ac27b0038753f0c5489c806bc8a0de2a72823
Andrey Lushnikov <lushnikov@chromium.org>
2019-02-07 11:15:21 -0800

    chore(testrunner): introduce verbose mode

--- test/test.js
+++ test/test.js
@@ -131,6 +131,8 @@ if (process.env.CI && testRunner.hasFocusedTestsOrSuites()) {
 }
 
 new Reporter(testRunner, {
+  verbose: process.argv.includes('--verbose'),
+  summary: !process.argv.includes('--verbose'),
   projectFolder: utils.projectRoot(),
   showSlowTests: process.env.CI ? 5 : 0,
 });
