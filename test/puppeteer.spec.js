fbf91cca982aff9549d593fa85aa137278e6847c
Andrey Lushnikov <aslushnikov@gmail.com>
2019-02-21 10:07:13 -0800

    test(firefox): move AX tests to Chrome-only (#4042)
    Let's not focus on AX for now for Firefox.

--- test/puppeteer.spec.js
+++ test/puppeteer.spec.js
@@ -117,7 +117,6 @@ module.exports.addTests = ({testRunner, product, puppeteer, Errors, DeviceDescri
 
       // Page-level tests that are given a browser, a context and a page.
       // Each test is launched in a new browser context.
-      require('./accessibility.spec.js').addTests(testOptions);
       require('./browser.spec.js').addTests(testOptions);
       require('./click.spec.js').addTests(testOptions);
       require('./cookies.spec.js').addTests(testOptions);
@@ -140,6 +139,7 @@ module.exports.addTests = ({testRunner, product, puppeteer, Errors, DeviceDescri
       require('./waittask.spec.js').addTests(testOptions);
       require('./worker.spec.js').addTests(testOptions);
       if (CHROME) {
+        require('./accessibility.spec.js').addTests(testOptions);
         require('./CDPSession.spec.js').addTests(testOptions);
         require('./coverage.spec.js').addTests(testOptions);
         require('./chromiumonly.spec.js').addTests(testOptions);
