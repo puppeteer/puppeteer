a6d8ecce6fbf3582359f6e75feae6e8d637d40c3
Joel Einbinder <joel.einbinder@gmail.com>
2019-02-27 15:13:17 -0800

    fix(firefox): keyboard tests (#4082)

--- test/mouse.spec.js
+++ test/mouse.spec.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+const os = require('os');
 
 function dimensions() {
   const rect = document.querySelector('textarea').getBoundingClientRect();
@@ -99,7 +100,7 @@ module.exports.addTests = function({testRunner, expect, FFOX}) {
       await page.hover('#button-6');
       expect(await page.evaluate(() => document.querySelector('button:hover').id)).toBe('button-6');
     });
-    it_fails_ffox('should set modifier keys on click', async({page, server}) => {
+    it('should set modifier keys on click', async({page, server}) => {
       await page.goto(server.PREFIX + '/input/scrollable.html');
       await page.evaluate(() => document.querySelector('#button-3').addEventListener('mousedown', e => window.lastEvent = e, true));
       const modifiers = {'Shift': 'shiftKey', 'Control': 'ctrlKey', 'Alt': 'altKey', 'Meta': 'metaKey'};
