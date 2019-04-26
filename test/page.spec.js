9a1867b0a1f767dea7a630d37cc3dd0eb0ee35ba
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-04-26 20:06:39 -0300

    test(page): add failing test for complex objects in exposeFunction

--- test/page.spec.js
+++ test/page.spec.js
@@ -656,6 +656,16 @@ module.exports.addTests = function({testRunner, expect, headless, puppeteer, CHR
       });
       expect(result).toBe(15);
     });
+
+    xit('should work with complex objects', async({page, server}) => {
+      await page.exposeFunction('complexObject', function(a, b) {
+        return {x: a.x + b.x};
+      });
+      const result = await page.evaluate(async function() {
+        return complexObject({x: 5}, {x: 2});
+      });
+      expect(result.x).toBe(7);
+    });
   });
 
   describe('Page.Events.PageError', function() {
