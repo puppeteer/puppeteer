6072b668ffae308b800316b9af46e807e2930960
Mathias Bynens <mathias@qiwi.be>
2019-09-17 15:00:59 +0200

    feat(api): add page.emulateTimezone(timezoneId)
    This enables dynamically changing the timezone of the page.

--- docs/api.md
+++ docs/api.md
@@ -109,6 +109,7 @@
   * [page.deleteCookie(...cookies)](#pagedeletecookiecookies)
   * [page.emulate(options)](#pageemulateoptions)
   * [page.emulateMedia(mediaType)](#pageemulatemediamediatype)
+  * [page.emulateTimezone(timezoneId)](#pageemulatetimezonetimezoneid)
   * [page.evaluate(pageFunction[, ...args])](#pageevaluatepagefunction-args)
   * [page.evaluateHandle(pageFunction[, ...args])](#pageevaluatehandlepagefunction-args)
   * [page.evaluateOnNewDocument(pageFunction[, ...args])](#pageevaluateonnewdocumentpagefunction-args)
@@ -1278,6 +1279,10 @@ List of all available devices is available in the source code: [DeviceDescriptor
 
 #### page.emulateMedia(mediaType)
 - `mediaType` <?[string]> Changes the CSS media type of the page. The only allowed values are `'screen'`, `'print'` and `null`. Passing `null` disables media emulation.
+- returns: <[Promise]>
+
+#### page.emulateTimezone(timezoneId)
+- `timezoneId` <?[string]> Changes the timezone of the page. See [ICU’s `metaZones.txt`](https://cs.chromium.org/chromium/src/third_party/icu/source/data/misc/metaZones.txt?rcl=faee8bc70570192d82d2978a71e2a615788597d1) for a list of supported timezone IDs. Passing `null` disables timezone emulation.
 - returns: <[Promise]>
 
 #### page.evaluate(pageFunction[, ...args])
--- test/emulation.spec.js
+++ test/emulation.spec.js
@@ -114,4 +114,29 @@ module.exports.addTests = function({testRunner, expect, puppeteer}) {
       expect(error.message).toBe('Unsupported media type: bad');
     });
   });
+
+  describe('Page.emulateTimezone', function() {
+    it('should work', async({page, server}) => {
+      const date = new Date(1479579154987);
+      await page.emulateTimezone('America/Jamaica');
+      expect(await page.evaluate(() => date.toString())).toBe('Sat Nov 19 2016 13:12:34 GMT-0500 (Eastern Standard Time)');
+
+      await page.emulateTimezone('Pacific/Honolulu');
+      expect(await page.evaluate(() => date.toString())).toBe('Sat Nov 19 2016 08:12:34 GMT-1000 (Hawaii-Aleutian Standard Time)');
+
+      await page.emulateTimezone('America/Buenos_Aires');
+      expect(await page.evaluate(() => date.toString())).toBe('Sat Nov 19 2016 15:12:34 GMT-0300 (Argentina Standard Time)');
+
+      await page.emulateTimezone('Europe/Berlin');
+      expect(await page.evaluate(() => date.toString())).toBe('Sat Nov 19 2016 19:12:34 GMT+0100 (Central European Standard Time)');
+    });
+
+    fit('should throw for invalid timezone IDs', async({page, server}) => {
+      let error = null;
+      await page.emulateTimezone('Foo/Bar').catch(e => error = e);
+      expect(error.message).toBe('Invalid timezone ID: Foo/Bar');
+      await page.emulateTimezone('Baz/Qux').catch(e => error = e);
+      expect(error.message).toBe('Invalid timezone ID: Baz/Qux');
+    });
+  });
 };
