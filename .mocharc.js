91c16a97b91903f458279d4021fe2bde5c945c7d
Jack Franklin <jacktfranklin@chromium.org>
2020-04-08 16:33:00 +0100

    up timeouts to what they were previously

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,5 @@ module.exports = {
   file: ['./test/mocha-utils.js'],
   spec: 'test/*.spec.js',
   reporter: 'dot',
-  timeout: process.env.PUPPETEER_PRODUCT === 'firefox' ? 10000 : 5000,
+  timeout: process.env.PUPPETEER_PRODUCT === 'firefox' ? 15 * 1000 : 10 * 1000,
 };
