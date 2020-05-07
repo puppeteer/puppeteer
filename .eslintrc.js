5577ec827456bd773fb83106c35b09f874194af8
Jack Franklin <jacktfranklin@chromium.org>
2020-05-07 10:42:15 +0100

    Configure ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,33 +13,29 @@ module.exports = {
         "unicorn"
     ],
 
+    "extends": [
+        "plugin:prettier/recommended"
+    ],
+
     "rules": {
+        // Error if files are not formatted with Prettier correctly.
+        "prettier/prettier": 2,
         // syntax preferences
         "quotes": [2, "single", {
             "avoidEscape": true,
             "allowTemplateLiterals": true
         }],
-        "semi": 2,
-        "no-extra-semi": 2,
-        "comma-style": [2, "last"],
-        "wrap-iife": [2, "inside"],
         "spaced-comment": [2, "always", {
             "markers": ["*"]
         }],
         "eqeqeq": [2],
-        "arrow-body-style": [2, "as-needed"],
         "accessor-pairs": [2, {
             "getWithoutSet": false,
             "setWithoutGet": false
         }],
-        "brace-style": [2, "1tbs", {"allowSingleLine": true}],
-        "curly": [2, "multi-or-nest", "consistent"],
-        "object-curly-spacing": [2, "never"],
         "new-parens": 2,
         "func-call-spacing": 2,
-        "arrow-parens": [2, "as-needed"],
         "prefer-const": 2,
-        "quote-props": [2, "consistent"],
 
         // anti-patterns
         "no-var": 2,
@@ -67,37 +63,6 @@ module.exports = {
         // es2015 features
         "require-yield": 2,
         "template-curly-spacing": [2, "never"],
-
-        // spacing details
-        "space-infix-ops": 2,
-        "space-in-parens": [2, "never"],
-        "space-before-function-paren": [2, "never"],
-        "no-whitespace-before-property": 2,
-        "keyword-spacing": [2, {
-            "overrides": {
-                "if": {"after": true},
-                "else": {"after": true},
-                "for": {"after": true},
-                "while": {"after": true},
-                "do": {"after": true},
-                "switch": {"after": true},
-                "return": {"after": true}
-            }
-        }],
-        "arrow-spacing": [2, {
-            "after": true,
-            "before": true
-        }],
-
-        // file whitespace
-        "no-multiple-empty-lines": [2, {"max": 2}],
-        "no-mixed-spaces-and-tabs": 2,
-        "no-trailing-spaces": 2,
-        "linebreak-style": [ process.platform === "win32" ? 0 : 2, "unix" ],
-        "indent": [2, 2, { "SwitchCase": 1, "CallExpression": {"arguments": 2}, "MemberExpression": 2 }],
-        "key-spacing": [2, {
-            "beforeColon": false
-        }],
 
         // ensure we don't have any it.only or describe.only in prod
         "mocha/no-exclusive-tests": "error",
