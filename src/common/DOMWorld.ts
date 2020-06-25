f6ef80525929c64dbdc1aeb695923ac14aaba385
Jack Franklin <jacktfranklin@chromium.org>
2020-06-25 10:19:10 +0100

    chore(agnostic): Migrate DOMWorld (#6054)
    DOMWorld only needs to use Node's `fs` module if you're adding a
    filepath as a script/style tag. We can detect this case and run the
    `require` inline such that in a browser this code won't execute.

--- src/common/DOMWorld.ts
+++ src/common/DOMWorld.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import * as fs from 'fs';
 import { assert } from './assert';
 import { helper } from './helper';
 import { LifecycleWatcher, PuppeteerLifeCycleEvent } from './LifecycleWatcher';
@@ -25,12 +24,11 @@ import { TimeoutSettings } from './TimeoutSettings';
 import { MouseButtonInput } from './Input';
 import { FrameManager, Frame } from './FrameManager';
 import { getQueryHandlerAndSelector, QueryHandler } from './QueryHandler';
+import { isNode } from '../environment';
 
 // This predicateQueryHandler is declared here so that TypeScript knows about it
 // when it is used in the predicate function below.
 declare const predicateQueryHandler: QueryHandler;
-
-const readFileAsync = helper.promisify(fs.readFile);
 
 export interface WaitForSelectorOptions {
   visible?: boolean;
@@ -232,8 +230,15 @@ export class DOMWorld {
   }
 
   /**
-   * @param {!{url?: string, path?: string, content?: string, type?: string}} options
-   * @returns {!Promise<!ElementHandle>}
+   * Adds a script tag into the current context.
+   *
+   * @remarks
+   *
+   * You can pass a URL, filepath or string of contents. Note that when running Puppeteer
+   * in a browser environment you cannot pass a filepath and should use either
+   * `url` or `content`.
+   *
+   * @param options
    */
   async addScriptTag(options: {
     url?: string;
@@ -254,6 +259,14 @@ export class DOMWorld {
     }
 
     if (path !== null) {
+      if (!isNode) {
+        throw new Error(
+          'Cannot pass a filepath to addScriptTag in the browser environment.'
+        );
+      }
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const fs = require('fs');
+      const readFileAsync = helper.promisify(fs.readFile);
       let contents = await readFileAsync(path, 'utf8');
       contents += '//# sourceURL=' + path.replace(/\n/g, '');
       const context = await this.executionContext();
@@ -304,6 +317,17 @@ export class DOMWorld {
     }
   }
 
+  /**
+   * Adds a style tag into the current context.
+   *
+   * @remarks
+   *
+   * You can pass a URL, filepath or string of contents. Note that when running Puppeteer
+   * in a browser environment you cannot pass a filepath and should use either
+   * `url` or `content`.
+   *
+   * @param options
+   */
   async addStyleTag(options: {
     url?: string;
     path?: string;
@@ -320,6 +344,14 @@ export class DOMWorld {
     }
 
     if (path !== null) {
+      if (!isNode) {
+        throw new Error(
+          'Cannot pass a filepath to addStyleTag in the browser environment.'
+        );
+      }
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const fs = require('fs');
+      const readFileAsync = helper.promisify(fs.readFile);
       let contents = await readFileAsync(path, 'utf8');
       contents += '/*# sourceURL=' + path.replace(/\n/g, '') + '*/';
       const context = await this.executionContext();
