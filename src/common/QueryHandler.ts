cc7f1fd063bf73dbb40cf08059283b78aaa28516
Johan Bay <jobay@google.com>
2020-10-07 10:43:46 +0200

    docs(queryhandler): add custom query handler docs (#6476)

--- src/common/QueryHandler.ts
+++ src/common/QueryHandler.ts
@@ -41,6 +41,17 @@ export interface InternalQueryHandler {
   ) => Promise<JSHandle>;
 }
 
+/**
+ * Contains two functions `queryOne` and `queryAll` that can
+ * be {@link Puppeteer.__experimental_registerCustomQueryHandler | registered}
+ * as alternative querying strategies. The functions `queryOne` and `queryAll`
+ * are executed in the page context.  `queryOne` should take an `Element` and a
+ * selector string as argument and return a single `Element` or `null` if no
+ * element is found. `queryAll` takes the same arguments but should instead
+ * return a `NodeListOf<Element>` or `Array<Element>` with all the elements
+ * that match the given query selector.
+ * @public
+ */
 export interface CustomQueryHandler {
   queryOne?: (element: Element | Document, selector: string) => Element | null;
   queryAll?: (
@@ -104,6 +115,9 @@ const _defaultHandler = makeQueryHandler({
 const _builtInHandlers = new Map([['aria', ariaHandler]]);
 const _queryHandlers = new Map(_builtInHandlers);
 
+/**
+ * @internal
+ */
 export function registerCustomQueryHandler(
   name: string,
   handler: CustomQueryHandler
@@ -121,7 +135,7 @@ export function registerCustomQueryHandler(
 }
 
 /**
- * @param {string} name
+ * @internal
  */
 export function unregisterCustomQueryHandler(name: string): void {
   if (_queryHandlers.has(name) && !_builtInHandlers.has(name)) {
@@ -129,16 +143,25 @@ export function unregisterCustomQueryHandler(name: string): void {
   }
 }
 
+/**
+ * @internal
+ */
 export function customQueryHandlerNames(): string[] {
   return [..._queryHandlers.keys()].filter(
     (name) => !_builtInHandlers.has(name)
   );
 }
 
+/**
+ * @internal
+ */
 export function clearCustomQueryHandlers(): void {
   customQueryHandlerNames().forEach(unregisterCustomQueryHandler);
 }
 
+/**
+ * @internal
+ */
 export function getQueryHandlerAndSelector(
   selector: string
 ): { updatedSelector: string; queryHandler: InternalQueryHandler } {
