c08b506107ff5fc26df761bc4a0ecd5eb188a7b3
Jack Franklin <jacktfranklin@chromium.org>
2020-10-22 11:27:13 +0100

    chore: list all tests

--- mocha-config/puppeteer-unit-tests.js
+++ mocha-config/puppeteer-unit-tests.js
@@ -21,9 +21,6 @@ module.exports = {
   require: ['./test/mocha-ts-require', './test/mocha-utils.ts'],
   spec: 'test/*.spec.ts',
   extension: ['js', 'ts'],
-  // parallel: process.env.CI && !process.env.COVERAGE,
-  // retry twice more, so we run each test up to 3 times if needed.
-  // retries: process.env.CI ? 2 : 0,
   retries: 0,
   timeout: 25 * 1000,
   reporter: 'list',
