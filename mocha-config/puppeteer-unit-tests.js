e6b8c77d94a54dd1e8de561cc33f571c26e7b05d
Jack Franklin <jacktfranklin@chromium.org>
2020-10-22 14:37:28 +0100

    chore: fix travis config (#6537)

--- mocha-config/puppeteer-unit-tests.js
+++ mocha-config/puppeteer-unit-tests.js
@@ -21,8 +21,8 @@ module.exports = {
   require: ['./test/mocha-ts-require', './test/mocha-utils.ts'],
   spec: 'test/*.spec.ts',
   extension: ['js', 'ts'],
-  parallel: process.env.CI && !process.env.COVERAGE,
-  // retry twice more, so we run each test up to 3 times if needed.
   retries: process.env.CI ? 2 : 0,
+  parallel: !!process.env.PARALLEL,
   timeout: 25 * 1000,
+  reporter: process.env.CI ? 'spec' : 'dot',
 };
