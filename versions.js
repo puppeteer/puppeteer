df7b4998539f6c61facfd36ac54abd475cd79393
Maksim Sadym <sadym@chromium.org>
2021-02-02 15:57:02 +0000

    dummy commit

--- versions.js
+++ versions.js
@@ -17,6 +17,7 @@
 const versionsPerRelease = new Map([
   // This is a mapping from Chromium version => Puppeteer version.
   // In Chromium roll patches, use 'NEXT' for the Puppeteer version.
+
   ['90.0.4403.0', 'NEXT'],
   ['89.0.4389.0', 'v6.0.0'],
   ['88.0.4298.0', 'v5.5.0'],
