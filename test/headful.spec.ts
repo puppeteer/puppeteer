e602aa1a0a40b750f69122ea239bb1daf7840592
Maksim Sadym <sadym@chromium.org>
2021-02-05 13:38:20 +0000

    fix(screenshot): don't show viewport size overlay
    * Add test for a clip screenshot.

--- test/headful.spec.ts
+++ test/headful.spec.ts
@@ -68,6 +68,8 @@ describeChromeOnly('headful tests', function () {
   });
 
   describe('HEADFUL', function () {
+    // Currently the test fails on the environments with scale factor != 1.
+    // https://github.com/puppeteer/puppeteer/issues/6823
     itFailsFirefox(
       'headfull with devtools should take fullPage screenshot',
       async () => {
@@ -95,12 +97,58 @@ describeChromeOnly('headful tests', function () {
         await browser.close();
 
         // Assertion is done after the browser is closed.
+        expect(screenshot1).toBeGolden('screenshot-grid-fullpage.png');
+        expect(screenshot2).toBeGolden('screenshot-grid-fullpage.png');
+        expect(screenshot3).toBeGolden('screenshot-grid-fullpage.png');
+      }
+    );
+    // Currently the test fails on the environments with scale factor != 1.
+    // https://github.com/puppeteer/puppeteer/issues/6823
+    itFailsFirefox(
+      'headfull with devtools should take clip screenshot',
+      async () => {
+        const { server, puppeteer } = getTestState();
 
-        // TODO: compare with `screenshot-grid-fullpage.png` after `scale` issue is fixed.
-        // https://github.com/puppeteer/puppeteer/issues/6823
-        expect(screenshot1).toBeGolden('headful-screenshot-grid-fullpage.png');
-        expect(screenshot2).toBeGolden('headful-screenshot-grid-fullpage.png');
-        expect(screenshot3).toBeGolden('headful-screenshot-grid-fullpage.png');
+        const browser = await puppeteer.launch(headfulDevToolsOptions);
+        const page = await browser.newPage();
+        await page.setViewport({ width: 500, height: 500 });
+        await page.goto(server.PREFIX + '/grid.html');
+
+        // Sometimes the first screenshot is taken before the size overlay is shown.
+        // 3 screenshots are taken to verify size overlay is not there.
+        const screenshot1 = await page.screenshot({
+          clip: {
+            x: 0,
+            y: 0,
+            width: 500,
+            height: 500,
+          },
+        });
+
+        const screenshot2 = await page.screenshot({
+          clip: {
+            x: 0,
+            y: 0,
+            width: 500,
+            height: 500,
+          },
+        });
+
+        const screenshot3 = await page.screenshot({
+          clip: {
+            x: 0,
+            y: 0,
+            width: 500,
+            height: 500,
+          },
+        });
+
+        await browser.close();
+
+        // Assertion is done after the browser is closed.
+        expect(screenshot1).toBeGolden('screenshot-sanity.png');
+        expect(screenshot2).toBeGolden('screenshot-sanity.png');
+        expect(screenshot3).toBeGolden('screenshot-sanity.png');
       }
     );
     it('background_page target type should be available', async () => {
