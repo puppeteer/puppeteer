0f88b258c5d663b0c5d94a05b585bdb811711da8
David Barton <barton.david.sv@gmail.com>
2021-02-07 11:44:42 +0100

    fix: linter error

--- utils/doclint/cli.js
+++ utils/doclint/cli.js
@@ -128,9 +128,11 @@ async function run() {
     clearExit = false;
   }
   console.log(`${errors.length} failures, ${warnings.length} warnings.`);
-  
+
   if (!clearExit && !process.env.GITHUB_ACTIONS)
-    console.log('\nIs your lib/ directory up to date? You might need to `npm run tsc`.\n');
+  console.log(
+    '\nIs your lib/ directory up to date? You might need to `npm run tsc`.\n'
+  );
 
   const runningTime = Date.now() - startTime;
   console.log(`DocLint Finished in ${runningTime / 1000} seconds`);
