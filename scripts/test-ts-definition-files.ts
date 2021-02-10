6ab6504f94dfad748acaef77fcba819995622aae
Jack Franklin <jacktfranklin@chromium.org>
2021-02-10 09:07:12 +0000

    fix: debugging failures

--- scripts/test-ts-definition-files.ts
+++ scripts/test-ts-definition-files.ts
@@ -28,15 +28,6 @@ const EXPECTED_ERRORS = new Map<string, string[]>([
     ],
   ],
   [
-    'js-cjs-import-cjs-output',
-    [
-      "bad.js(5,35): error TS2551: Property 'launh' does not exist on type",
-      "bad.js(7,29): error TS2551: Property 'devics' does not exist on type",
-      'bad.js(11,39): error TS2554: Expected 0 arguments, but got 1.',
-      "bad.js(15,9): error TS2322: Type 'ElementHandle<Element> | null' is not assignable to type 'ElementHandle<HTMLElement>'",
-    ],
-  ],
-  [
     'js-esm-import-cjs-output',
     [
       "bad.js(5,35): error TS2551: Property 'launh' does not exist on type",
@@ -56,6 +47,15 @@ const EXPECTED_ERRORS = new Map<string, string[]>([
   ],
   [
     'js-esm-import-esm-output',
+    [
+      "bad.js(5,35): error TS2551: Property 'launh' does not exist on type",
+      "bad.js(7,29): error TS2551: Property 'devics' does not exist on type",
+      'bad.js(11,39): error TS2554: Expected 0 arguments, but got 1.',
+      "bad.js(15,9): error TS2322: Type 'ElementHandle<Element> | null' is not assignable to type 'ElementHandle<HTMLElement>'",
+    ],
+  ],
+  [
+    'js-cjs-import-cjs-output',
     [
       "bad.js(5,35): error TS2551: Property 'launh' does not exist on type",
       "bad.js(7,29): error TS2551: Property 'devics' does not exist on type",
@@ -88,7 +88,7 @@ function compileAndCatchErrors(projectLocation) {
   let tsErrorMesssage: string[] = [];
   let result;
   try {
-    result = execSync(`cd ${projectLocation} && npm run compile`, {
+    result = execSync(`cd ${projectLocation} && npm run compile -- --verbose`, {
       encoding: 'utf-8',
       stdio: 'pipe', // means the output isn't logged by default
     });
@@ -131,7 +131,7 @@ function testProject(folder: string) {
   const expectedErrors = EXPECTED_ERRORS.get(folder) || [];
   if (
     result.tsErrorMesssage.find(
-      (line) => line.includes('good.ts') || line.includes('good.js')
+      (line) => line.includes('good.ts(') || line.includes('good.js(')
     )
   ) {
     console.error(
@@ -140,7 +140,7 @@ function testProject(folder: string) {
     process.exit(1);
   }
   const errorsInTsMessage = result.tsErrorMesssage.filter(
-    (line) => line.includes('bad.ts') || line.includes('bad.js')
+    (line) => line.includes('bad.ts(') || line.includes('bad.js(')
   );
   const expectedErrorsThatHaveOccurred = new Set<string>();
   const unexpectedErrors = errorsInTsMessage.filter((message) => {
