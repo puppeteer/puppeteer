42393cfcb965c161d723a4d52cc7abc9bed1e983
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-04-02 15:19:08 +0530

    Update FileChooser.ts

--- src/common/FileChooser.ts
+++ src/common/FileChooser.ts
@@ -34,7 +34,7 @@ import { assert } from './assert.js';
  * **NOTE** In browsers, only one file chooser can be opened at a time.
  * All file choosers must be accepted or canceled. Not doing so will prevent
  * subsequent file choosers from app/earing.
- * @internal
+ * @public
  */
 export class FileChooser {
   private _element: ElementHandle;
