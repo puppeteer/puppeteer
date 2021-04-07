1e76d44f128071d8d72c86baf4752a39ebecb41b
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-04-07 14:29:29 +0530

    Fixed warning in 27 of "Browser.ts"
    Exported "BrowserCloseCallback" to fix one of the warnings in Browser.ts.

--- src/common/Browser.ts
+++ src/common/Browser.ts
@@ -24,7 +24,7 @@ import { Page } from './Page.js';
 import { ChildProcess } from 'child_process';
 import { Viewport } from './PuppeteerViewport.js';
 
-type BrowserCloseCallback = () => Promise<void> | void;
+export type BrowserCloseCallback = () => Promise<void> | void;
 
 const WEB_PERMISSION_TO_PROTOCOL_PERMISSION = new Map<
   Permission,
