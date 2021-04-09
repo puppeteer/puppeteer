b481e326ebd82958f9cdd7166efbdc3fcbe3c426
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-04-09 16:01:49 +0530

    docs: exported class CustomError from Errors.ts

--- src/common/Errors.ts
+++ src/common/Errors.ts
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/**
-*@public
-*/
 export class CustomError extends Error {
   constructor(message: string) {
     super(message);
