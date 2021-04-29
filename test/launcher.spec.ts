11b6d12b25ee000b27790084d3065c2d273ff303
Peng-Yu Chen <pengyu@libstarrify.so>
2021-04-29 11:39:21 +0100

    test(launcher): formatting the newly added waitForInitialPage test case
    To resolve the linter errors.

--- test/launcher.spec.ts
+++ test/launcher.spec.ts
@@ -430,21 +430,24 @@ describe('Launcher specs', function () {
         expect(screenshot).toBeInstanceOf(Buffer);
         await browser.close();
       });
-      itChromeOnly('should launch Chrome properly with --no-startup-window and waitForInitialPage=false', async function () {
-        const { defaultBrowserOptions, puppeteer } = getTestState();
-        const options = {
-          args: ['--no-startup-window'],
-          waitForInitialPage: false,
-          // This is needed to prevent Puppeteer from adding an initial blank page.
-          // See also https://github.com/puppeteer/puppeteer/blob/ad6b736039436fcc5c0a262e5b575aa041427be3/src/node/Launcher.ts#L200
-          ignoreDefaultArgs: true,
-          ...defaultBrowserOptions,
+      itChromeOnly(
+        'should launch Chrome properly with --no-startup-window and waitForInitialPage=false',
+        async () => {
+          const { defaultBrowserOptions, puppeteer } = getTestState();
+          const options = {
+            args: ['--no-startup-window'],
+            waitForInitialPage: false,
+            // This is needed to prevent Puppeteer from adding an initial blank page.
+            // See also https://github.com/puppeteer/puppeteer/blob/ad6b736039436fcc5c0a262e5b575aa041427be3/src/node/Launcher.ts#L200
+            ignoreDefaultArgs: true,
+            ...defaultBrowserOptions,
+          };
+          const browser = await puppeteer.launch(options);
+          const pages = await browser.pages();
+          expect(pages.length).toBe(0);
+          await browser.close();
         }
-        const browser = await puppeteer.launch(options);
-        const pages = await browser.pages();
-        expect(pages.length).toBe(0);
-        await browser.close();
-      });
+      );
     });
 
     describe('Puppeteer.launch', function () {
