159d2835450697dabea6f9adf6e67d158b5b8ae3
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-05-24 18:44:51 +0530

    fix: modified comment for method product, platform and newPage (#7262)

--- src/common/Browser.ts
+++ src/common/Browser.ts
@@ -417,7 +417,8 @@ export class Browser extends EventEmitter {
   }
 
   /**
-   * Creates a {@link Page} in the default browser context.
+   * Promise which resolves to a new {@link Page} object. The Page is created in
+   * a default browser context.
    */
   async newPage(): Promise<Page> {
     return this._defaultContext.newPage();
