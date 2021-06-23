d68fa11659ac104188eacb2a2596a1b8104c9e23
tasneemkoushar <imatasneemkoushar@gmail.com>
2021-06-23 23:48:27 +0530

    fix: errors in script

--- utils/remove.js
+++ utils/remove.js
@@ -1,12 +1,12 @@
-var glob = require('glob');
+const glob = require('glob');
 const fs = require('fs');
 
-let files = glob.sync(`./new-docs/*.md`);
+const files = glob.sync(`./new-docs/*.md`);
 
 files.forEach(function(file){
-    let str = file;
-    let data = fs.readFileSync(str,'utf-8');
-          var newValue = data.replace(/<!-- -->/gim, '');
+    const str = file;
+    const data = fs.readFileSync(str,'utf-8');
+          const newValue = data.replace(/<!-- -->/gim, '');
           fs.writeFileSync(str, newValue, 'utf-8', function(err, data) {
                 if (err) throw err;
                   console.log('Done!');
