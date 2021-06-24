7c16c7560593f0025c3b29fc380e6bf529685639
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-06-24 11:03:22 +0530

    fixed errors in the script and class Page (#7364)
    * fix: added comments and a script
    
    * fix: solved the errors in script
    
    * fix: errors in script
    
    * fix: errors in page class
    
    * fix: errors in remove.js

---
+++ utils/remove.js
@@ -0,0 +1,12 @@
+const glob = require('glob');
+const fs = require('fs');
+const files = glob.sync(`./new-docs/*.md`);
+files.forEach(function(file){
+const str = file;
+const data = fs.readFileSync(str,'utf-8');
+const newValue = data.replace(/<!-- -->/gim, '');
+fs.writeFileSync(str, newValue, 'utf-8', function(err, data) {
+if (err) throw err;
+console.log('Done!');
+});
+});
