d381fa9b25276b767e780dc6963ec84af44b6f92
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-06-29 18:40:25 +0530

    fix: update utils/remove-tag.js
    Co-authored-by: Mathias Bynens <mathias@qiwi.be>

--- utils/remove-tag.js
+++ utils/remove-tag.js
@@ -20,14 +20,8 @@ const fs = require('fs');
 //look for all .md files in the given location.
 const files = glob.sync(`./new-docs/*.md`);
 
-files.forEach(function (file) {
-  const str = file;
-  const data = fs.readFileSync(str, 'utf-8');
-  const newValue = data.replace(/<!-- -->/gim, '');
-
-  //overwrite <!-- --> syntax with empty string  in.md files.
-  fs.writeFileSync(str, newValue, 'utf-8', function (err, data) {
-    if (err) throw err;
-    console.log('Done!');
-  });
-});
+for (const file of files) {
+  const content = fs.readFileSync(file, 'utf8');
+  const updated = content.replaceAll('<!-- -->', '');
+  fs.writeFileSync(file, updated);
+}
