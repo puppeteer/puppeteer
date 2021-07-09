d70f60e0619b8659d191fa492e3db4bc221ae982
Yusuke Iwaki <q7w8e9w8q7w8e9@yahoo.co.jp>
2021-07-09 21:43:42 +0900

    feat: add channel parameter for puppeteer.launch (#7389)
    This change adds a new `channel` parameter to `puppeteer.launch`. When specified, Puppeteer will search for the locally installed release channel of Google Chrome and use it to launch. Available values are `chrome`, `chrome-beta`, `chrome-canary`, `chrome-dev`. This parameter is mutually exclusive with `executablePath`.

--- src/node/LaunchOptions.ts
+++ src/node/LaunchOptions.ts
@@ -46,11 +46,21 @@ export interface BrowserLaunchArgumentOptions {
   args?: string[];
 }
 
+export type ChromeReleaseChannel =
+  | 'chrome'
+  | 'chrome-beta'
+  | 'chrome-canary'
+  | 'chrome-dev';
+
 /**
  * Generic launch options that can be passed when launching any browser.
  * @public
  */
 export interface LaunchOptions {
+  /**
+   * Chrome Release Channel
+   */
+  channel?: ChromeReleaseChannel;
   /**
    * Path to a browser executable to use instead of the bundled Chromium. Note
    * that Puppeteer is only guaranteed to work with the bundled Chromium, so use
