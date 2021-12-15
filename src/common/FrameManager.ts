3c20ab411f9d9015bd9e879212e24826bceb3295
DimaShustal <dzmitry.shustal@gmail.com>
2021-12-15 16:16:15 +0300

    fix(page): Page.createIsolatedWorld error catching has been added
    when navigating, frames are destroyed and sending events leads to an error that crashes the whole process

--- src/common/FrameManager.ts
+++ src/common/FrameManager.ts
@@ -398,7 +398,7 @@ export class FrameManager extends EventEmitter {
             frameId: frame._id,
             worldName: name,
             grantUniveralAccess: true,
-          })
+          }).catch(console.error)
         )
     );
   }
