96c3f943b2f6e26bd871ecfcce71b6a33e214ebf
jrandolf <86539915+jrandolf@users.noreply.github.com>
2022-02-04 12:18:53 +0100

    feat(chromium): roll to Chromium 99.0.4844.16 (r961656) (#7960)
    Co-authored-by: Randolf Jung <jrandolf@chromium.org>

--- versions.js
+++ versions.js
@@ -17,6 +17,7 @@
 const versionsPerRelease = new Map([
   // This is a mapping from Chromium version => Puppeteer version.
   // In Chromium roll patches, use 'NEXT' for the Puppeteer version.
+  ['99.0.4844.16', 'NEXT'],
   ['98.0.4758.0', 'v13.1.0'],
   ['97.0.4692.0', 'v12.0.0'],
   ['93.0.4577.0', 'v10.2.0'],
