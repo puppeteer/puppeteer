0cf4832878731ffcfc84570315f326eb851d7629
jrandolf <101637635+jrandolf@users.noreply.github.com>
2022-07-01 16:00:03 +0200

    fix: remove unused imports (#8613)

---
+++ .eslintrc.types.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['unused-imports'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
