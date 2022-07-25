936ed8607ec0c3798d2b22b590d0be0ad361a888
Alex Rudenko <OrKoN@users.noreply.github.com>
2022-07-25 14:34:01 +0200

    feat: add Dockerfile (#8315)
    This PR adds an official Dockerfile for Puppeteer. The content of the Dockerfile is practically the same as documented in troubleshooting.md:
    
    1) It installs chrome-stable and dependencies via apt-get.
    2) it installs a local Puppeteer build into the docker user's home folder.
    3) configures required permissions for the user.
    4) outputs licenses into the THIRD_PARTY_NOTICES file.
    
    The local Puppeteer build is created by `docker/pack.sh` which is meant to be used in CI. This PR also includes a GitHub action that would build a docker image and run a smote test inside of it.
    
    The next step would be actually publishing the docker image from GitHub Actions to GitHub Registry.

---
+++ docker/test/smoke-test.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await browser.close();
+  console.log('done');
+})();
