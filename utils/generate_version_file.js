4c5dbdcedebb1425942293bd55d8222f579694ce
Randolf J <jrandolf@google.com>
2022-08-16 07:06:04 +0200

    chore: update `NEXT` during versioning

--- utils/generate_version_file.js
+++ utils/generate_version_file.js
@@ -1,9 +1,18 @@
 const {writeFileSync, readFileSync} = require('fs');
 const {join} = require('path');
 
+const version = require('../package.json').version;
+
 writeFileSync(
   join(__dirname, '../src/generated/version.ts'),
   readFileSync(join(__dirname, '../src/templates/version.ts.tmpl'), {
     encoding: 'utf-8',
-  }).replace('PACKAGE_VERSION', require('../package.json').version)
+  }).replace('PACKAGE_VERSION', version)
+);
+
+writeFileSync(
+  join(__dirname, '../versions.js'),
+  readFileSync(join(__dirname, '../versions.js'), {
+    encoding: 'utf-8',
+  }).replace('NEXT', `v${version}`)
 );
