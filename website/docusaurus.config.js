a4e444d2175d7be94409aa4b1472db73e76fdaee
Alex Rudenko <OrKoN@users.noreply.github.com>
2022-09-12 13:59:05 +0200

    docs: use swc for building the website (#8941)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -40,6 +40,25 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  webpack: {
+    jsLoader: isServer => {
+      return {
+        loader: require.resolve('swc-loader'),
+        options: {
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+              tsx: true,
+            },
+            target: 'es2017',
+          },
+          module: {
+            type: isServer ? 'commonjs' : 'es6',
+          },
+        },
+      };
+    },
+  },
   presets: [
     [
       'classic',
