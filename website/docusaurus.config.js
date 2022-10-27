ea068254cd6b40c8b6154ec93ba7b0fd0731a6d9
Nikolay Vitkov <nvitkov@chromium.org>
2022-10-27 13:17:08 +0200

    chore: Add GitHub Icon

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,6 +25,15 @@ const archivedVersions = require('./versionsArchived.json');
 const DOC_ROUTE_BASE_PATH = '/';
 const DOC_PATH = '../docs';
 
+const GitHubIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path fill="currentcolor" d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7
+     1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 
+     3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 
+     2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"/>
+  </svg>
+`;
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Puppeteer',
@@ -216,6 +225,13 @@ const config = {
             type: 'search',
             position: 'right',
           },
+          {
+            href: 'https://github.com/puppeteer/puppeteer',
+            position: 'right',
+            className: 'navbar__link--github',
+            'aria-label': 'GitHub repository',
+            html: GitHubIcon,
+          },
         ],
       },
       footer: {
@@ -235,6 +251,19 @@ const config = {
               {
                 label: 'YouTube',
                 href: 'https://goo.gle/devtools-youtube',
+              },
+            ],
+          },
+          {
+            title: 'Useful',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/puppeteer/puppeteer',
+              },
+              {
+                label: 'Changelog',
+                href: 'https://github.com/puppeteer/puppeteer/blob/main/packages/puppeteer/CHANGELOG.md',
               },
             ],
           },
