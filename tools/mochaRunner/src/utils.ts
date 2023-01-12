9e538c4936c6a84c2a961aff7e94ad9d624a69b7
Alexandra Borovova <aborovova@mozilla.com>
2023-01-12 16:54:35 +0100

    chore: filter expectations by the whole test name

--- tools/mochaRunner/src/utils.ts
+++ tools/mochaRunner/src/utils.ts
@@ -57,11 +57,11 @@ export function prettyPrintJSON(json: unknown): void {
 }
 
 export function filterByParameters(
-  expecations: TestExpectation[],
+  expectations: TestExpectation[],
   parameters: string[]
 ): TestExpectation[] {
   const querySet = new Set(parameters);
-  return expecations.filter(ex => {
+  return expectations.filter(ex => {
     return ex.parameters.every(param => {
       return querySet.has(param);
     });
@@ -69,22 +69,17 @@ export function filterByParameters(
 }
 
 /**
- * The last expectation that matches the startsWith filter wins.
+ * The last expectation that matches the filter wins.
  */
-export function findEffectiveExpecationForTest(
+export function findEffectiveExpectationForTest(
   expectations: TestExpectation[],
   result: MochaTestResult
 ): TestExpectation | undefined {
   return expectations
-    .filter(expecation => {
-      if (
-        getTestId(result.file, result.fullTitle).startsWith(
-          expecation.testIdPattern
-        )
-      ) {
-        return true;
-      }
-      return false;
+    .filter(expectation => {
+      return (
+        getTestId(result.file, result.fullTitle) === expectation.testIdPattern
+      );
     })
     .pop();
 }
@@ -106,7 +101,7 @@ export function getExpectationUpdates(
   const output: RecommendedExpecation[] = [];
 
   for (const pass of results.passes) {
-    const expectation = findEffectiveExpecationForTest(expecations, pass);
+    const expectation = findEffectiveExpectationForTest(expecations, pass);
     if (expectation && !expectation.expectations.includes('PASS')) {
       output.push({
         expectation,
@@ -117,7 +112,7 @@ export function getExpectationUpdates(
   }
 
   for (const failure of results.failures) {
-    const expectation = findEffectiveExpecationForTest(expecations, failure);
+    const expectation = findEffectiveExpectationForTest(expecations, failure);
     if (expectation) {
       if (
         !expectation.expectations.includes(getTestResultForFailure(failure))
