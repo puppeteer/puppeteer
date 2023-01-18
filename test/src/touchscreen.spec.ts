a8a1c6bc7c033d5ccf758811fa700dd889c95dab
charlieinitialdigital <122505548+charlieinitialdigital@users.noreply.github.com>
2023-01-18 17:03:47 +0800

    add press test function
    add press test function

--- test/src/touchscreen.spec.ts
+++ test/src/touchscreen.spec.ts
@@ -1,113 +1,24 @@
-/**
- * Copyright 2018 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import expect from 'expect';
-import { BoundingBox } from 'puppeteer';
-import { KnownDevices } from 'puppeteer';
-import {
-  getTestState,
-  setupTestBrowserHooks,
-  setupTestPageAndContextHooks,
-} from './mocha-utils.js';
-
-describe('Touchscreen', function () {
-  setupTestBrowserHooks();
-  setupTestPageAndContextHooks();
-
-  it('should tap the button', async () => {
-    const { page, server } = getTestState();
-    const iPhone = KnownDevices['iPhone 6']!;
-    await page.emulate(iPhone);
-    await page.goto(server.PREFIX + '/input/button.html');
-    await page.tap('button');
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).result;
-      })
-    ).toBe('Clicked');
-  });
-
-  it('should report touches', async () => {
-    const { page, server } = getTestState();
-    const iPhone = KnownDevices['iPhone 6']!;
-    await page.emulate(iPhone);
-    await page.goto(server.PREFIX + '/input/touches.html');
-    const button = (await page.$('button'))!;
-    await button.tap();
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).getResult();
-      })
-    ).toEqual(['Touchstart: 0', 'Touchend: 0']);
-  });
-
-  it('should report press', async () => {
-    const { page, server } = getTestState();
-    const iPhone = KnownDevices['iPhone 6']!;
-    await page.emulate(iPhone);
-    await page.goto(server.PREFIX + '/input/touches.html');
-    await page.touchscreen.press(0, 0, 5);
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).getResult();
-      })
-    ).toEqual(['Touchstart: 0', 'Touchend: 0']);
-  });
-
-  it('should report drag', async () => {
-    const { page, server } = getTestState();
-    const iPhone = KnownDevices['iPhone 6']!;
-    await page.emulate(iPhone);
-    await page.goto(server.PREFIX + '/input/touches-drag-and-drop.html');
-    await page.setViewport({
-      width: KnownDevices['iPhone 6'].viewport.width,
-      height: KnownDevices['iPhone 6'].viewport.height,
-      isMobile: true,
-      hasTouch: true,
-    });
-    const draggable = (await page.$('#drag'))!;
-    const dropzone = (await page.$('#drop'))!;
-    let dragObj = await draggable.boundingBox() as BoundingBox;
-    let dropObj = await dropzone.boundingBox() as BoundingBox;
-    let dragPosx = dragObj.x + dragObj.width / 2;
-    let dragPosy = dragObj.y + dragObj.height / 2;
-    let dropPosx = dropObj.x + dropObj.width / 2;
-    let dropPosy = dropObj.y + dropObj.height / 2;
-    await page.touchscreen.drag({ x: dragPosx, y: dragPosy }, { x: dropPosx, y: dropPosy })
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).didDragStart;
-      })
-    ).toBe(true);
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).didDragEnter;
-      })
-    ).toBe(true);
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).didDragOver;
-      })
-    ).toBe(true);
-    expect(
-      await page.evaluate(() => {
-        return (globalThis as any).didDrop;
-      })
-    ).toBe(true);
-  });
-
-
-});
+   it('should report press', async () => {
+     const {page, server} = getTestState();
+     const iPhone = KnownDevices['iPhone 6']!;
++    await page.setViewport({
++      width: KnownDevices['iPhone 6'].viewport.width,
++      height: KnownDevices['iPhone 6'].viewport.height,
++      isMobile: true,
++      hasTouch: true,
++    });
+     await page.emulate(iPhone);
+-    await page.goto(server.PREFIX + '/input/touches.html');
+-    await page.touchscreen.press(0, 0, 5);
++    await page.goto(server.PREFIX + '/input/touches-press.html');
++    await page.touchscreen.press(100, 100, 1000);
+     expect(
+       await page.evaluate(() => {
+-        return (globalThis as any).getResult();
++        return (globalThis as any).timer;
+       })
+-    ).toEqual(['Touchstart: 0', 'Touchend: 0']);
++    ).toBe(1);
+   });
+ 
+   it('should report drag', async () => {
