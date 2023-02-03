c8bb11adfcf1537032730a91baa3c36a6e324926
charlieinitialdigital <122505548+charlieinitialdigital@users.noreply.github.com>
2023-02-03 18:59:21 +0800

    feat: add touchstart, touchmove and touchend methods (#9622)

---
+++ docs/api/puppeteer.elementhandle.touchstart.md
@@ -0,0 +1,23 @@
+---
+sidebar_label: ElementHandle.touchStart
+---
+
+# ElementHandle.touchStart() method
+
+#### Signature:
+
+```typescript
+class ElementHandle {
+  touchStart(this: ElementHandle<Element>): Promise<void>;
+}
+```
+
+## Parameters
+
+| Parameter | Type                                                         | Description |
+| --------- | ------------------------------------------------------------ | ----------- |
+| this      | [ElementHandle](./puppeteer.elementhandle.md)&lt;Element&gt; |             |
+
+**Returns:**
+
+Promise&lt;void&gt;
--- packages/puppeteer-core/src/common/Input.ts
+++ packages/puppeteer-core/src/common/Input.ts
@@ -670,12 +670,40 @@ export class Touchscreen {
    * @param y - Vertical position of the tap.
    */
   async tap(x: number, y: number): Promise<void> {
+    await this.touchStart(x, y);
+    await this.touchEnd();
+  }
+
+  /**
+   * Dispatches a `touchstart` event.
+   * @param x - Horizontal position of the tap.
+   * @param y - Vertical position of the tap.
+   */
+  async touchStart(x: number, y: number): Promise<void> {
     const touchPoints = [{x: Math.round(x), y: Math.round(y)}];
     await this.#client.send('Input.dispatchTouchEvent', {
       type: 'touchStart',
       touchPoints,
       modifiers: this.#keyboard._modifiers,
     });
+  }
+  /**
+   * Dispatches a `touchMove` event.
+   * @param x - Horizontal position of the move.
+   * @param y - Vertical position of the move.
+   */
+  async touchMove(x: number, y: number): Promise<void> {
+    const movePoints = [{x: Math.round(x), y: Math.round(y)}];
+    await this.#client.send('Input.dispatchTouchEvent', {
+      type: 'touchMove',
+      touchPoints: movePoints,
+      modifiers: this.#keyboard._modifiers,
+    });
+  }
+  /**
+   * Dispatches a `touchend` event.
+   */
+  async touchEnd(): Promise<void> {
     await this.#client.send('Input.dispatchTouchEvent', {
       type: 'touchEnd',
       touchPoints: [],
