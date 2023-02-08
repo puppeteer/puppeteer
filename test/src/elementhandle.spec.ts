15488f0997b190d1d46968e34bbb37149afa844b
Nikolay Vitkov <nvitkov@chromium.org>
2023-02-08 15:40:21 +0100

    chore: Fix Tests

--- test/src/elementhandle.spec.ts
+++ test/src/elementhandle.spec.ts
@@ -15,7 +15,7 @@
  */
 
 import expect from 'expect';
-import {ElementHandle} from 'puppeteer-core/internal/common/ElementHandle.js';
+import {ElementHandle} from 'puppeteer-core/internal/api/ElementHandle.js';
 import sinon from 'sinon';
 import {
   getTestState,
