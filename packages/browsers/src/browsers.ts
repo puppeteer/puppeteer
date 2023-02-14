76d22f6c7d052e2a2e7ab44821e617d80c8a1d45
Alex Rudenko <alexrudenko@chromium.org>
2023-02-14 11:32:37 +0100

    chore: add a basic CLI for fetching the browsers

---
+++ packages/browsers/src/browsers.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import {CLI} from './CLI.js';
+
+new CLI().run(process.argv);
---
+++ packages/browsers/test/src/cli.spec.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2023 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {CLI} from '../../lib/cjs/CLI.js';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import assert from 'assert';
+
+describe('CLI', function () {
+  this.timeout(60000);
+
+  let tmpDir = '/tmp/puppeteer-browsers-test';
+  const testChromeRevision = '1083080';
+  const testFirefoxRevision = '111.0a1';
+
+  beforeEach(() => {
+    tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'puppeteer-browsers-test'));
+  });
+
+  afterEach(() => {
+    fs.rmSync(tmpDir, {recursive: true});
+  });
+
+  it('should download Chromium binaries', async () => {
+    await new CLI(tmpDir).run([
+      'npx',
+      '@puppeteer/browsers',
+      'install',
+      `chrome@${testChromeRevision}`,
+      `--path=${tmpDir}`,
+      '--platform=linux',
+    ]);
+    assert.ok(
+      fs.existsSync(path.join(tmpDir, 'chrome', `linux-${testChromeRevision}`))
+    );
+  });
+
+  it('should download Firefox binaries', async () => {
+    await new CLI(tmpDir).run([
+      'npx',
+      '@puppeteer/browsers',
+      'install',
+      `firefox@${testFirefoxRevision}`,
+      `--path=${tmpDir}`,
+      '--platform=linux',
+    ]);
+    assert.ok(
+      fs.existsSync(
+        path.join(tmpDir, 'firefox', `linux-${testFirefoxRevision}`)
+      )
+    );
+  });
+});
