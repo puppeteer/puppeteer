e3f8ff23c35370d061477748560b7baafa599f3b
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-04-14 10:51:35 +0200

    chore: fix build on win (#10022)

---
+++ tools/chmod.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2023 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fs from 'fs';
+
+/**
+ * Calls chmod with the mode in argv[2] on paths in argv[3...length-1].
+ */
+
+const mode = process.argv[2];
+
+for (let i = 3; i < process.argv.length; i++) {
+  fs.chmodSync(process.argv[i], mode);
+}
