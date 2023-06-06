9cb713ff3a1c8487d65831e22010d3b77903b77c
Alex Rudenko <alexrudenko@chromium.org>
2023-06-06 11:50:58 +0200

    chore: try increasing the timeout on windows

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -22,6 +22,6 @@ module.exports = {
   exit: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
   parallel: !!process.env.PARALLEL,
-  timeout: 10_000,
+  timeout: process.platform === 'win32' ? 20_000 : 10_000,
   reporter: process.env.CI ? 'spec' : 'dot',
 };
