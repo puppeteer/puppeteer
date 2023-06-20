9b1b6b061d68352dd114a500eb70203b4ca726fd
Randolf <jrandolf@google.com>
2023-06-20 16:00:12 +0200

    fix: add parenthesis to JS values in interpolateFunction
    This fixes an issue where minifiers would inline PLACEHOLDER(...) calls causing invalid JS to be evaluated at runtime.

---
+++ test/src/function.spec.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2018 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import expect from 'expect';
+import {interpolateFunction} from 'puppeteer-core/internal/util/Function.js';
+
+describe('Function', function () {
+  describe('interpolateFunction', function () {
+    it('should work', async () => {
+      const test = interpolateFunction(
+        () => {
+          const test = PLACEHOLDER('test') as () => number;
+          return test();
+        },
+        {test: `() => 5`}
+      );
+      expect(test()).toBe(5);
+    });
+    it('should work inlined', async () => {
+      const test = interpolateFunction(
+        () => {
+          // Note the parenthesis will be removed by the typescript compiler.
+          return (PLACEHOLDER('test') as () => number)();
+        },
+        {test: `() => 5`}
+      );
+      expect(test()).toBe(5);
+    });
+  });
+});
