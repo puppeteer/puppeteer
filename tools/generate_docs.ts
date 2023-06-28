10fa352102c55dd2587c31a489df105a1fe7507e
Nikolay Vitkov <34244704+Lightning00Blade@users.noreply.github.com>
2023-06-28 10:07:14 +0200

    docs: add `ng-schematics` page (#10459)

--- tools/generate_docs.ts
+++ tools/generate_docs.ts
@@ -52,7 +52,7 @@ function spliceIntoSection(
 }
 
 (async () => {
-  const job1 = job('', async ({inputs, outputs}) => {
+  const copyMain = job('Copy main page', async ({inputs, outputs}) => {
     await copyFile(inputs[0]!, outputs[0]!);
   })
     .inputs(['README.md'])
@@ -60,50 +60,53 @@ function spliceIntoSection(
     .build();
 
   // Chrome Versions
-  const job2 = job('', async ({inputs, outputs}) => {
-    let content = await readFile(inputs[2]!, {encoding: 'utf8'});
-    const versionModulePath = join('..', inputs[0]!);
-    const {versionsPerRelease} = await import(versionModulePath);
-    const versionsArchived = JSON.parse(await readFile(inputs[1]!, 'utf8'));
+  const updateSupportedList = job(
+    'Update the Chrome supported list',
+    async ({inputs, outputs}) => {
+      let content = await readFile(inputs[2]!, {encoding: 'utf8'});
+      const versionModulePath = join('..', inputs[0]!);
+      const {versionsPerRelease} = await import(versionModulePath);
+      const versionsArchived = JSON.parse(await readFile(inputs[1]!, 'utf8'));
 
-    // Generate versions
-    const buffer: string[] = [];
-    for (const [chromiumVersion, puppeteerVersion] of versionsPerRelease) {
-      if (puppeteerVersion === 'NEXT') {
-        continue;
-      }
-      if (versionsArchived.includes(puppeteerVersion.substring(1))) {
-        if (semver.gte(puppeteerVersion, '20.0.0')) {
+      // Generate versions
+      const buffer: string[] = [];
+      for (const [chromiumVersion, puppeteerVersion] of versionsPerRelease) {
+        if (puppeteerVersion === 'NEXT') {
+          continue;
+        }
+        if (versionsArchived.includes(puppeteerVersion.substring(1))) {
+          if (semver.gte(puppeteerVersion, '20.0.0')) {
+            buffer.push(
+              `  * [Chrome for Testing](https://goo.gle/chrome-for-testing) ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://pptr.dev/${puppeteerVersion.slice(
+                1
+              )})`
+            );
+          } else {
+            buffer.push(
+              `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://github.com/puppeteer/puppeteer/blob/${puppeteerVersion}/docs/api/index.md)`
+            );
+          }
+        } else if (semver.lt(puppeteerVersion, '15.0.0')) {
           buffer.push(
-            `  * [Chrome for Testing](https://goo.gle/chrome-for-testing) ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://pptr.dev/${puppeteerVersion.slice(
+            `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://github.com/puppeteer/puppeteer/blob/${puppeteerVersion}/docs/api.md)`
+          );
+        } else if (semver.gte(puppeteerVersion, '15.3.0')) {
+          buffer.push(
+            `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://pptr.dev/${puppeteerVersion.slice(
               1
             )})`
           );
         } else {
           buffer.push(
-            `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://github.com/puppeteer/puppeteer/blob/${puppeteerVersion}/docs/api/index.md)`
+            `  * Chromium ${chromiumVersion} - Puppeteer ${puppeteerVersion}`
           );
         }
-      } else if (semver.lt(puppeteerVersion, '15.0.0')) {
-        buffer.push(
-          `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://github.com/puppeteer/puppeteer/blob/${puppeteerVersion}/docs/api.md)`
-        );
-      } else if (semver.gte(puppeteerVersion, '15.3.0')) {
-        buffer.push(
-          `  * Chromium ${chromiumVersion} - [Puppeteer ${puppeteerVersion}](https://pptr.dev/${puppeteerVersion.slice(
-            1
-          )})`
-        );
-      } else {
-        buffer.push(
-          `  * Chromium ${chromiumVersion} - Puppeteer ${puppeteerVersion}`
-        );
       }
-    }
-    content = spliceIntoSection('version', content, buffer.join('\n'));
+      content = spliceIntoSection('version', content, buffer.join('\n'));
 
-    await writeFile(outputs[0]!, content);
-  })
+      await writeFile(outputs[0]!, content);
+    }
+  )
     .inputs([
       'versions.js',
       'website/versionsArchived.json',
@@ -112,14 +115,27 @@ function spliceIntoSection(
     .outputs(['docs/chromium-support.md'])
     .build();
 
-  await Promise.all([job1, job2]);
+  const copyNgSchematics = job(
+    'Copy @puppeteer/ng-schematics main page',
+    async ({inputs, outputs}) => {
+      await copyFile(inputs[0]!, outputs[0]!);
+    }
+  )
+    .inputs(['packages/ng-schematics/README.md'])
+    .outputs(['docs/ng-schematics.md'])
+    .build();
+
+  await Promise.all([copyMain, updateSupportedList, copyNgSchematics]);
 
   // Generate documentation
-  const puppeteerDocs = job('', async ({inputs, outputs}) => {
-    await rm(outputs[0]!, {recursive: true, force: true});
-    generateDocs(inputs[0]!, outputs[0]!);
-    spawnAndLog('prettier', '--ignore-path', 'none', '--write', 'docs');
-  })
+  const puppeteerDocs = job(
+    'Generate Puppeteer API markdown',
+    async ({inputs, outputs}) => {
+      await rm(outputs[0]!, {recursive: true, force: true});
+      generateDocs(inputs[0]!, outputs[0]!);
+      spawnAndLog('prettier', '--ignore-path', 'none', '--write', 'docs');
+    }
+  )
     .inputs([
       'docs/puppeteer.api.json',
       'tools/internal/custom_markdown_documenter.ts',
@@ -127,11 +143,14 @@ function spliceIntoSection(
     .outputs(['docs/api'])
     .build();
 
-  const browsersDocs = job('', async ({inputs, outputs}) => {
-    await rm(outputs[0]!, {recursive: true, force: true});
-    generateDocs(inputs[0]!, outputs[0]!);
-    spawnAndLog('prettier', '--ignore-path', 'none', '--write', 'docs');
-  })
+  const browsersDocs = job(
+    'Generate @puppeteer/browsers API markdown',
+    async ({inputs, outputs}) => {
+      await rm(outputs[0]!, {recursive: true, force: true});
+      generateDocs(inputs[0]!, outputs[0]!);
+      spawnAndLog('prettier', '--ignore-path', 'none', '--write', 'docs');
+    }
+  )
     .inputs([
       'docs/browsers.api.json',
       'tools/internal/custom_markdown_documenter.ts',
@@ -141,11 +160,14 @@ function spliceIntoSection(
 
   await Promise.all([puppeteerDocs, browsersDocs]);
 
-  await job('', async ({inputs, outputs}) => {
-    const readme = await readFile(inputs[1]!, 'utf-8');
-    const index = await readFile(inputs[0]!, 'utf-8');
-    await writeFile(outputs[0]!, index.replace('# API Reference\n', readme));
-  })
+  await job(
+    'Update main @puppeteer/browsers page',
+    async ({inputs, outputs}) => {
+      const readme = await readFile(inputs[1]!, 'utf-8');
+      const index = await readFile(inputs[0]!, 'utf-8');
+      await writeFile(outputs[0]!, index.replace('# API Reference\n', readme));
+    }
+  )
     .inputs(['docs/browsers-api/index.md', 'packages/browsers/README.md'])
     .outputs(['docs/browsers-api/index.md'])
     .build();
