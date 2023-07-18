e6265ee3539c6613af2430e04ed9704d0bc0c6dd
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-07-18 11:33:24 +0200

    test: run new headless for the docker test (#10568)

--- docker/test/smoke-test.js
+++ docker/test/smoke-test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: 'new'});
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await browser.close();
