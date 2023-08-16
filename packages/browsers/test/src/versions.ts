3dfff5af6b8dcdc2ef3306ddb9e6b651d08b81f3
Mathias Bynens <mathias@qiwi.be>
2023-08-16 15:38:05 +0200

    chore: add script to update `versions.ts` (#10742)

--- packages/browsers/test/src/versions.ts
+++ packages/browsers/test/src/versions.ts
@@ -16,8 +16,6 @@
 
 export const testChromeBuildId = '113.0.5672.0';
 export const testChromiumBuildId = '1083080';
-// TODO: We can add a Cron job to auto-update on change.
-// Firefox keeps only `latest` version of Nightly builds.
 export const testFirefoxBuildId = '118.0a1';
 export const testChromeDriverBuildId = '115.0.5763.0';
 export const testChromeHeadlessShellBuildId = '118.0.5950.0';
