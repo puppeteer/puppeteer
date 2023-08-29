dca327f99f60bad0b1fabc6071f656f843ef7536
jrandolf <101637635+jrandolf@users.noreply.github.com>
2023-08-29 21:41:29 +0200

    chore: implement use-using rule (#10798)

--- tools/eslint/src/prettier-comments.js
+++ tools/eslint/src/prettier-comments.js
@@ -1,5 +1,26 @@
+/**
+ * Copyright 2023 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @ts-nocheck
+// TODO: We should convert this to types.
+
 const prettier = require('@prettier/sync');
-const prettierConfig = require('./.prettierrc.cjs');
+
+const prettierConfigPath = '../../../.prettierrc.cjs';
+const prettierConfig = require(prettierConfigPath);
 
 const cleanupBlockComment = value => {
   return value
@@ -46,7 +67,7 @@ const buildBlockComment = (value, offset) => {
 /**
  * @type import("eslint").Rule.RuleModule
  */
-const rule = {
+const prettierCommentsRule = {
   meta: {
     type: 'suggestion',
     docs: {
@@ -85,8 +106,4 @@ const rule = {
   },
 };
 
-module.exports = {
-  rules: {
-    'prettier-comments': rule,
-  },
-};
+module.exports = prettierCommentsRule;
