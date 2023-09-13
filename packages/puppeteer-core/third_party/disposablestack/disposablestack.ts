c5e4f1f2bbbcc32f3236b15353714f994a1e766f
Randolf <jrandolf@google.com>
2023-09-13 12:04:13 +0200

    fix: use supported node range for types
    Using the supported node range for types ensures Puppeteer compiles to the correct types.

--- packages/puppeteer-core/third_party/disposablestack/disposablestack.ts
+++ packages/puppeteer-core/third_party/disposablestack/disposablestack.ts
@@ -1,6 +1,26 @@
 import 'disposablestack/auto';
 
 declare global {
+  interface SymbolConstructor {
+    /**
+     * A method that is used to release resources held by an object. Called by the semantics of the `using` statement.
+     */
+    readonly dispose: unique symbol;
+
+    /**
+     * A method that is used to asynchronously release resources held by an object. Called by the semantics of the `await using` statement.
+     */
+    readonly asyncDispose: unique symbol;
+  }
+
+  interface Disposable {
+    [Symbol.dispose](): void;
+  }
+
+  interface AsyncDisposable {
+    [Symbol.asyncDispose](): PromiseLike<void>;
+  }
+
   class DisposableStack implements Disposable {
     constructor();
 
