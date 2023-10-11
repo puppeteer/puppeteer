f4361f433c34e5b67e55ea3f95681b3e306e95ea
Alex Rudenko <alexrudenko@chromium.org>
2023-10-11 10:58:56 +0200

    fix: update capture stack trace

--- packages/puppeteer-core/src/common/Errors.ts
+++ packages/puppeteer-core/src/common/Errors.ts
@@ -26,7 +26,7 @@ export class CustomError extends Error {
   constructor(message?: string) {
     super(message);
     this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
+    Error.captureStackTrace?.(this, this.constructor);
   }
 }
 
