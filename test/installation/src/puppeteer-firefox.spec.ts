cfe4b8b0cd25b6d6a98c11af36815c78d78cf300
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-10-13 10:03:14 +0200

    ci: optimize installation tests and inspect code (#11150)

--- test/installation/src/puppeteer-firefox.spec.ts
+++ test/installation/src/puppeteer-firefox.spec.ts
@@ -16,32 +16,37 @@
 
 import assert from 'assert';
 import {readdir} from 'fs/promises';
+import {platform} from 'os';
 import {join} from 'path';
 
 import {configureSandbox} from './sandbox.js';
 import {readAsset} from './util.js';
 
-describe('`puppeteer` with Firefox', () => {
-  configureSandbox({
-    dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
-    env: cwd => {
-      return {
-        PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
-        PUPPETEER_PRODUCT: 'firefox',
-      };
-    },
-  });
+// Skipping this test on Windows as windows runners are much slower.
+(platform() === 'win32' ? describe.skip : describe)(
+  '`puppeteer` with Firefox',
+  () => {
+    configureSandbox({
+      dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
+      env: cwd => {
+        return {
+          PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
+          PUPPETEER_PRODUCT: 'firefox',
+        };
+      },
+    });
 
-  it('evaluates CommonJS', async function () {
-    const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
-    assert.equal(files.length, 1);
-    assert.equal(files[0], 'firefox');
-    const script = await readAsset('puppeteer-core', 'requires.cjs');
-    await this.runScript(script, 'cjs');
-  });
+    it('evaluates CommonJS', async function () {
+      const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
+      assert.equal(files.length, 1);
+      assert.equal(files[0], 'firefox');
+      const script = await readAsset('puppeteer-core', 'requires.cjs');
+      await this.runScript(script, 'cjs');
+    });
 
-  it('evaluates ES modules', async function () {
-    const script = await readAsset('puppeteer-core', 'imports.js');
-    await this.runScript(script, 'mjs');
-  });
-});
+    it('evaluates ES modules', async function () {
+      const script = await readAsset('puppeteer-core', 'imports.js');
+      await this.runScript(script, 'mjs');
+    });
+  }
+);
--- test/installation/src/puppeteer.spec.ts
+++ test/installation/src/puppeteer.spec.ts
@@ -17,6 +17,7 @@
 import assert from 'assert';
 import {readdirSync} from 'fs';
 import {readdir} from 'fs/promises';
+import {platform} from 'os';
 import {join} from 'path';
 
 import {configureSandbox} from './sandbox.js';
@@ -46,57 +47,65 @@ describe('`puppeteer`', () => {
   });
 });
 
-describe('`puppeteer` with PUPPETEER_DOWNLOAD_PATH', () => {
-  configureSandbox({
-    dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
-    env: cwd => {
-      return {
-        PUPPETEER_DOWNLOAD_PATH: join(cwd, '.cache', 'puppeteer'),
-      };
-    },
-  });
+// Skipping this test on Windows as windows runners are much slower.
+(platform() === 'win32' ? describe.skip : describe)(
+  '`puppeteer` with PUPPETEER_DOWNLOAD_PATH',
+  () => {
+    configureSandbox({
+      dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
+      env: cwd => {
+        return {
+          PUPPETEER_DOWNLOAD_PATH: join(cwd, '.cache', 'puppeteer'),
+        };
+      },
+    });
 
-  it('evaluates', async function () {
-    const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
-    assert.equal(files.length, 1);
-    assert.equal(files[0], 'chrome');
+    it('evaluates', async function () {
+      const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
+      assert.equal(files.length, 1);
+      assert.equal(files[0], 'chrome');
 
-    const script = await readAsset('puppeteer', 'basic.js');
-    await this.runScript(script, 'mjs');
-  });
-});
+      const script = await readAsset('puppeteer', 'basic.js');
+      await this.runScript(script, 'mjs');
+    });
+  }
+);
 
-describe('`puppeteer` clears cache', () => {
-  configureSandbox({
-    dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
-    env: cwd => {
-      return {
-        PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
-      };
-    },
-  });
+// Skipping this test on Windows as windows runners are much slower.
+(platform() === 'win32' ? describe.skip : describe)(
+  '`puppeteer` clears cache',
+  () => {
+    configureSandbox({
+      dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
+      env: cwd => {
+        return {
+          PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
+        };
+      },
+    });
 
-  it('evaluates', async function () {
-    assert.equal(
-      readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
-      1
-    );
+    it('evaluates', async function () {
+      assert.equal(
+        readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
+        1
+      );
 
-    await this.runScript(
-      await readAsset('puppeteer', 'installCanary.js'),
-      'mjs'
-    );
+      await this.runScript(
+        await readAsset('puppeteer', 'installCanary.js'),
+        'mjs'
+      );
 
-    assert.equal(
-      readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
-      2
-    );
+      assert.equal(
+        readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
+        2
+      );
 
-    await this.runScript(await readAsset('puppeteer', 'trimCache.js'), 'mjs');
+      await this.runScript(await readAsset('puppeteer', 'trimCache.js'), 'mjs');
 
-    assert.equal(
-      readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
-      1
-    );
-  });
-});
+      assert.equal(
+        readdirSync(join(this.sandbox, '.cache', 'puppeteer', 'chrome')).length,
+        1
+      );
+    });
+  }
+);
