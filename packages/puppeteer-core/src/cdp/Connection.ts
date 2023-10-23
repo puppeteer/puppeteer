11d68e8ac6bbf729fbd555c41b045f15483c43a7
Alex Rudenko <alexrudenko@chromium.org>
2023-10-23 10:33:27 +0200

    fix: remove import cycle in connection

--- packages/puppeteer-core/src/cdp/Connection.ts
+++ packages/puppeteer-core/src/cdp/Connection.ts
@@ -22,12 +22,12 @@ import {
   type CDPSession,
   type CDPSessionEvents,
 } from '../api/CDPSession.js';
+import {CallbackRegistry} from '../common/CallbackRegistry.js';
 import type {ConnectionTransport} from '../common/ConnectionTransport.js';
 import {debug} from '../common/Debug.js';
-import {ProtocolError, TargetCloseError} from '../common/Errors.js';
+import {TargetCloseError} from '../common/Errors.js';
 import {EventEmitter} from '../common/EventEmitter.js';
-import {debugError} from '../common/util.js';
-import {Deferred} from '../util/Deferred.js';
+import {createProtocolErrorMessage} from '../util/ErrorLike.js';
 
 import {CdpCDPSession} from './CDPSession.js';
 
@@ -38,159 +38,6 @@ const debugProtocolReceive = debug('puppeteer:protocol:RECV ◀');
  * @public
  */
 export type {ConnectionTransport, ProtocolMapping};
-
-/**
- * @internal
- */
-type GetIdFn = () => number;
-
-/**
- * @internal
- */
-function createIncrementalIdGenerator(): GetIdFn {
-  let id = 0;
-  return (): number => {
-    return ++id;
-  };
-}
-
-/**
- * @internal
- */
-export class Callback {
-  #id: number;
-  #error = new ProtocolError();
-  #deferred = Deferred.create<unknown>();
-  #timer?: ReturnType<typeof setTimeout>;
-  #label: string;
-
-  constructor(id: number, label: string, timeout?: number) {
-    this.#id = id;
-    this.#label = label;
-    if (timeout) {
-      this.#timer = setTimeout(() => {
-        this.#deferred.reject(
-          rewriteError(
-            this.#error,
-            `${label} timed out. Increase the 'protocolTimeout' setting in launch/connect calls for a higher timeout if needed.`
-          )
-        );
-      }, timeout);
-    }
-  }
-
-  resolve(value: unknown): void {
-    clearTimeout(this.#timer);
-    this.#deferred.resolve(value);
-  }
-
-  reject(error: Error): void {
-    clearTimeout(this.#timer);
-    this.#deferred.reject(error);
-  }
-
-  get id(): number {
-    return this.#id;
-  }
-
-  get promise(): Deferred<unknown> {
-    return this.#deferred;
-  }
-
-  get error(): ProtocolError {
-    return this.#error;
-  }
-
-  get label(): string {
-    return this.#label;
-  }
-}
-
-/**
- * Manages callbacks and their IDs for the protocol request/response communication.
- *
- * @internal
- */
-export class CallbackRegistry {
-  #callbacks = new Map<number, Callback>();
-  #idGenerator = createIncrementalIdGenerator();
-
-  create(
-    label: string,
-    timeout: number | undefined,
-    request: (id: number) => void
-  ): Promise<unknown> {
-    const callback = new Callback(this.#idGenerator(), label, timeout);
-    this.#callbacks.set(callback.id, callback);
-    try {
-      request(callback.id);
-    } catch (error) {
-      // We still throw sync errors synchronously and clean up the scheduled
-      // callback.
-      callback.promise
-        .valueOrThrow()
-        .catch(debugError)
-        .finally(() => {
-          this.#callbacks.delete(callback.id);
-        });
-      callback.reject(error as Error);
-      throw error;
-    }
-    // Must only have sync code up until here.
-    return callback.promise.valueOrThrow().finally(() => {
-      this.#callbacks.delete(callback.id);
-    });
-  }
-
-  reject(id: number, message: string, originalMessage?: string): void {
-    const callback = this.#callbacks.get(id);
-    if (!callback) {
-      return;
-    }
-    this._reject(callback, message, originalMessage);
-  }
-
-  _reject(
-    callback: Callback,
-    errorMessage: string | ProtocolError,
-    originalMessage?: string
-  ): void {
-    let error: ProtocolError;
-    let message: string;
-    if (errorMessage instanceof ProtocolError) {
-      error = errorMessage;
-      error.cause = callback.error;
-      message = errorMessage.message;
-    } else {
-      error = callback.error;
-      message = errorMessage;
-    }
-
-    callback.reject(
-      rewriteError(
-        error,
-        `Protocol error (${callback.label}): ${message}`,
-        originalMessage
-      )
-    );
-  }
-
-  resolve(id: number, value: unknown): void {
-    const callback = this.#callbacks.get(id);
-    if (!callback) {
-      return;
-    }
-    callback.resolve(value);
-  }
-
-  clear(): void {
-    for (const callback of this.#callbacks.values()) {
-      // TODO: probably we can accept error messages as params.
-      this._reject(callback, new TargetCloseError('Target closed'));
-    }
-    this.#callbacks.clear();
-  }
-}
 
 /**
  * @public
@@ -412,35 +259,6 @@ export class Connection extends EventEmitter<CDPSessionEvents> {
   ): Promise<CDPSession> {
     return await this._createSession(targetInfo, false);
   }
-}
-
-/**
- * @internal
- */
-export function createProtocolErrorMessage(object: {
-  error: {message: string; data: any; code: number};
-}): string {
-  let message = `${object.error.message}`;
-  // TODO: remove the type checks when we stop connecting to BiDi with a CDP
-  // client.
-  if (
-    object.error &&
-    typeof object.error === 'object' &&
-    'data' in object.error
-  ) {
-    message += ` ${object.error.data}`;
-  }
-  return message;
-}
-
-function rewriteError(
-  error: ProtocolError,
-  message: string,
-  originalMessage?: string
-): Error {
-  error.message = message;
-  error.originalMessage = originalMessage ?? error.originalMessage;
-  return error;
 }
 
 /**
