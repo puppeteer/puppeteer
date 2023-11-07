08615f848c87ffa44bde0e0c2ecef07545ba4d39
Nikolay Vitkov <34244704+Lightning00Blade@users.noreply.github.com>
2023-11-07 08:20:44 +0100

    test: add installation test for BiDi (#11311)

--- test/installation/src/puppeteer-firefox.spec.ts
+++ test/installation/src/puppeteer-firefox.spec.ts
@@ -36,17 +36,26 @@ import {readAsset} from './util.js';
       },
     });
 
-    it('evaluates CommonJS', async function () {
-      const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
-      assert.equal(files.length, 1);
-      assert.equal(files[0], 'firefox');
-      const script = await readAsset('puppeteer-core', 'requires.cjs');
-      await this.runScript(script, 'cjs');
+    describe('with CDP', () => {
+      it('evaluates CommonJS', async function () {
+        const files = await readdir(join(this.sandbox, '.cache', 'puppeteer'));
+        assert.equal(files.length, 1);
+        assert.equal(files[0], 'firefox');
+        const script = await readAsset('puppeteer-core', 'requires.cjs');
+        await this.runScript(script, 'cjs');
+      });
+
+      it('evaluates ES modules', async function () {
+        const script = await readAsset('puppeteer-core', 'imports.js');
+        await this.runScript(script, 'mjs');
+      });
     });
 
-    it('evaluates ES modules', async function () {
-      const script = await readAsset('puppeteer-core', 'imports.js');
-      await this.runScript(script, 'mjs');
+    describe('with WebDriverBiDi', () => {
+      it('evaluates ES modules', async function () {
+        const script = await readAsset('puppeteer', 'bidi.js');
+        await this.runScript(script, 'mjs');
+      });
     });
   }
 );
