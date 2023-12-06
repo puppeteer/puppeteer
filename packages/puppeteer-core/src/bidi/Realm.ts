0354511c63d3df3d007eb23d913e37462218748f
Alex Rudenko <alexrudenko@chromium.org>
2023-12-06 13:57:33 +0100

    test: add a test for eval followed by a mouse event

--- packages/puppeteer-core/src/bidi/Realm.ts
+++ packages/puppeteer-core/src/bidi/Realm.ts
@@ -176,11 +176,13 @@ export class BidiRealm extends EventEmitter<Record<EventType, any>> {
         : `${functionDeclaration}\n${sourceUrlComment}\n`;
       responsePromise = this.connection.send('script.callFunction', {
         functionDeclaration,
-        arguments: await Promise.all(
-          args.map(arg => {
-            return BidiSerializer.serialize(sandbox, arg);
-          })
-        ),
+        arguments: args.length
+          ? await Promise.all(
+              args.map(arg => {
+                return BidiSerializer.serialize(sandbox, arg);
+              })
+            )
+          : [],
         target: this.target,
         resultOwnership,
         awaitPromise: true,
