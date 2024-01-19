5b19b25285b9171426b3d43388dda32c254026ac
Nikolay Vitkov <nvitkov@chromium.org>
2024-01-19 10:58:54 +0100

    chore: remove unused code and dep

--- packages/browsers/.mocharc.cjs
+++ packages/browsers/.mocharc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   logLevel: 'debug',
   spec: 'test/build/**/*.spec.js',
+  require: ['./test/build/mocha-utils.js'],
   exit: !!process.env.CI,
   reporter: 'spec',
   timeout: 10_000,
