b4b96dfd8374b9713cca5309c308249d7e186180
richestrinh <107906449+richestrinh@users.noreply.github.com>
2024-01-22 23:27:07 +0000

    Revert "feat: EsLint rule to check/add license header"
    This reverts commit 50ef690aedcfcb6bbf583c795552dfbeee7a50e0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -149,8 +149,6 @@ module.exports = {
     'rulesdir/prettier-comments': 'error',
     // Enforces consistent file extension
     'rulesdir/extensions': 'error',
-    // Enforces license headers on files (.ts,.mjs,.cjs)
-    'rulesdir/check-license': 'error',
   },
   overrides: [
     {
