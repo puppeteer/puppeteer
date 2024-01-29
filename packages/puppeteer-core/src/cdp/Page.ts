53b1376a410a8b84e99f0d6cca320e74f8275560
Maksim Sadym <sadym@chromium.org>
2024-01-29 15:57:04 +0100

    Switch to `storage.getCookies`

--- packages/puppeteer-core/src/cdp/Page.ts
+++ packages/puppeteer-core/src/cdp/Page.ts
@@ -575,10 +575,8 @@ export class CdpPage extends Page {
 
   override async cookies(...urls: string[]): Promise<Cookie[]> {
     const originalCookies = (
-      await this.#primaryTargetClient.send('Network.getCookies', {
-        urls: urls.length ? urls : [this.url()],
-      })
-    ).cookies;
+      await this.#primaryTargetClient.send('Storage.getCookies', {})
+    ).cookies.filter(c => urls.some(u => Page.testCookieUrlMatch(c, u)));
 
     const unsupportedCookieAttributes = ['priority'];
     const filterUnsupportedAttributes = (
