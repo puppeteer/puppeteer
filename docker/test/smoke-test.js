51d26f0b43589a88f05308f772b4b98060cf658a
Alex Rudenko <OrKoN@users.noreply.github.com>
2024-02-12 12:28:47 +0100

    test: fix the headless flag in the smoke test

--- docker/test/smoke-test.js
+++ docker/test/smoke-test.js
@@ -6,7 +6,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: 'new'});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await browser.close();
