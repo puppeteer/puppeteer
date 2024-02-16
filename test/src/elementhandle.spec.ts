dec6b22b99444fbe3a74aa55f0f217ed9202687e
Alex Rudenko <alexrudenko@chromium.org>
2024-02-16 08:58:09 +0100

    test: remove redundant expectations

--- test/src/elementhandle.spec.ts
+++ test/src/elementhandle.spec.ts
@@ -34,18 +34,14 @@ describe('ElementHandle specs', function () {
       expect(box).toEqual({x: 100, y: 50, width: 50, height: 50});
     });
     it('should handle nested frames', async () => {
-      const {page, server, isChrome} = await getTestState();
+      const {page, server} = await getTestState();
 
       await page.setViewport({width: 500, height: 500});
       await page.goto(server.PREFIX + '/frames/nested-frames.html');
       const nestedFrame = page.frames()[1]!.childFrames()[1]!;
       using elementHandle = (await nestedFrame.$('div'))!;
       const box = await elementHandle.boundingBox();
-      if (isChrome) {
-        expect(box).toEqual({x: 28, y: 182, width: 264, height: 18});
-      } else {
-        expect(box).toEqual({x: 28, y: 182, width: 254, height: 18});
-      }
+      expect(box).toEqual({x: 28, y: 182, width: 300, height: 18});
     });
     it('should return null for invisible elements', async () => {
       const {page} = await getTestState();
