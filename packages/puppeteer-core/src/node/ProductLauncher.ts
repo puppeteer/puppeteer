f93fbefdc13472e8da94fa692d99c5b919f49cce
dickorydock <dickorydock@users.noreply.github.com>
2024-03-05 02:18:00 +0000

    Update ProductLauncher.ts
    These URLs do not work with a period at the end

--- packages/puppeteer-core/src/node/ProductLauncher.ts
+++ packages/puppeteer-core/src/node/ProductLauncher.ts
@@ -435,14 +435,14 @@ export abstract class ProductLauncher {
             `Could not find Chrome (ver. ${this.puppeteer.browserRevision}). This can occur if either\n` +
               ' 1. you did not perform an installation before running the script (e.g. `npx puppeteer browsers install chrome`) or\n' +
               ` 2. your cache path is incorrectly configured (which is: ${this.puppeteer.configuration.cacheDirectory}).\n` +
-              'For (2), check out our guide on configuring puppeteer at https://pptr.dev/guides/configuration.'
+              'For (2), check out our guide on configuring puppeteer at https://pptr.dev/guides/configuration'
           );
         case 'firefox':
           throw new Error(
             `Could not find Firefox (rev. ${this.puppeteer.browserRevision}). This can occur if either\n` +
               ' 1. you did not perform an installation for Firefox before running the script (e.g. `npx puppeteer browsers install firefox`) or\n' +
               ` 2. your cache path is incorrectly configured (which is: ${this.puppeteer.configuration.cacheDirectory}).\n` +
-              'For (2), check out our guide on configuring puppeteer at https://pptr.dev/guides/configuration.'
+              'For (2), check out our guide on configuring puppeteer at https://pptr.dev/guides/configuration'
           );
       }
     }
