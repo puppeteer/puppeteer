4f422e60a65011459f38eae3def3cf9d78161d29
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-03-11 07:54:55 -0300

    test: improve `target.asPage` tests (#12060)

--- test/src/cdp/devtools.spec.ts
+++ test/src/cdp/devtools.spec.ts
@@ -67,9 +67,9 @@ describe('DevTools', function () {
         return 2 * 3;
       })
     ).toBe(6);
-    expect(await browser.pages()).toContainEqual(page);
+    expect(await browser.pages()).toContain(page);
   });
-  it('target.page() should return a DevTools page if asPage is used', async function () {
+  it('target.page() should return Page when calling asPage on DevTools target', async function () {
     const {puppeteer} = await getTestState({skipLaunch: true});
     const originalBrowser = await launchBrowser(launchOptions);
 
@@ -87,7 +87,8 @@ describe('DevTools', function () {
         return 2 * 3;
       })
     ).toBe(6);
-    expect(await browser.pages()).toContainEqual(page);
+    // The page won't be part of browser.pages() if a custom isPageTarget is not provided
+    expect(await browser.pages()).not.toContain(page);
   });
   it('should open devtools when "devtools: true" option is given', async () => {
     const browser = await launchBrowser(
