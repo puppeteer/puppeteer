fb0d7252e46c1f73a7c492f5db1d09fbbb310175
Alex Rudenko <OrKoN@users.noreply.github.com>
2024-04-29 12:58:20 +0200

    refactor: split entrypoint imports (#12350)

--- packages/puppeteer-core/src/puppeteer-core.ts
+++ packages/puppeteer-core/src/puppeteer-core.ts
@@ -4,19 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export type {Protocol} from 'devtools-protocol';
-
-export * from './api/api.js';
-export * from './cdp/cdp.js';
-export * from './common/common.js';
-export * from './node/node.js';
-export * from './revisions.js';
-export * from './util/util.js';
-
-/**
- * @deprecated Use the query handler API defined on {@link Puppeteer}
- */
-export * from './common/CustomQueryHandler.js';
+export * from './index.js';
 
 import {PuppeteerNode} from './node/PuppeteerNode.js';
 
