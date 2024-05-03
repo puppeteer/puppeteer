0af4664b8a9103b8cf746d799e7f60eaf49bd987
Alex Rudenko <OrKoN@users.noreply.github.com>
2024-05-03 10:31:21 +0200

    test: add canary specific expectation file (#12383)

---
+++ tools/merge-canary-test-expectations.mjs
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Modifies test/TestExpectations.json in place.
+
+import fs from 'fs';
+
+const source = 'test/TestExpectations.json';
+const testExpectations = JSON.parse(fs.readFileSync(source, 'utf-8'));
+const canaryTestExpectations = JSON.parse(
+  fs.readFileSync('test/CanaryTestExpectations.json', 'utf-8')
+);
+
+fs.writeFileSync(
+  source,
+  JSON.stringify([...testExpectations, ...canaryTestExpectations], null, 2)
+);
