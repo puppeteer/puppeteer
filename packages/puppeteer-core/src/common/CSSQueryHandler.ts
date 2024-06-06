12bc264e78b8ed5c6cec5e6633587ac702529b64
Alex Rudenko <alexrudenko@chromium.org>
2024-06-06 19:54:48 +0200

    refactor: move selector parsing to Node

---
+++ packages/puppeteer-core/src/common/CSSQueryHandler.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright 2023 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type PuppeteerUtil from '../injected/injected.js';
+
+import {QueryHandler} from './QueryHandler.js';
+
+/**
+ * @internal
+ */
+export class CSSQueryHandler extends QueryHandler {
+  static override querySelector = (
+    element: Node,
+    selector: string,
+    {cssQuerySelector}: PuppeteerUtil
+  ): Node | null => {
+    return cssQuerySelector(element, selector);
+  };
+  static override querySelectorAll = (
+    element: Node,
+    selector: string,
+    {cssQuerySelectorAll}: PuppeteerUtil
+  ): Iterable<Node> => {
+    return cssQuerySelectorAll(element, selector);
+  };
+}
