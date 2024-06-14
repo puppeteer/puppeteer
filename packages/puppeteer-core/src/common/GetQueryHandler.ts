255cedb639c94778ecfb352f37523e6f5ff8dd05
Alex Rudenko <alexrudenko@chromium.org>
2024-06-14 07:30:02 +0200

    fix: ensure selector parser falls back to CSS

--- packages/puppeteer-core/src/common/GetQueryHandler.ts
+++ packages/puppeteer-core/src/common/GetQueryHandler.ts
@@ -52,17 +52,25 @@ export function getQueryHandlerAndSelector(selector: string): {
       }
     }
   }
-  const [pSelector, isPureCSS, hasPseudoClasses] = parsePSelectors(selector);
-  if (isPureCSS) {
+  try {
+    const [pSelector, isPureCSS, hasPseudoClasses] = parsePSelectors(selector);
+    if (isPureCSS) {
+      return {
+        updatedSelector: selector,
+        selectorHasPseudoClasses: hasPseudoClasses,
+        QueryHandler: CSSQueryHandler,
+      };
+    }
+    return {
+      updatedSelector: JSON.stringify(pSelector),
+      selectorHasPseudoClasses: hasPseudoClasses,
+      QueryHandler: PQueryHandler,
+    };
+  } catch {
     return {
       updatedSelector: selector,
-      selectorHasPseudoClasses: hasPseudoClasses,
+      selectorHasPseudoClasses: false,
       QueryHandler: CSSQueryHandler,
     };
   }
-  return {
-    updatedSelector: JSON.stringify(pSelector),
-    selectorHasPseudoClasses: hasPseudoClasses,
-    QueryHandler: PQueryHandler,
-  };
 }
