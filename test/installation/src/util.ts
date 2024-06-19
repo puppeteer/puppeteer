a80f7cf9e3bbb0e31db7c2cded7ffdbcdfeb4910
Alex Rudenko <alexrudenko@chromium.org>
2024-06-19 12:41:05 +0200

    test: instrument execFile

--- test/installation/src/util.ts
+++ test/installation/src/util.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import type {ExecFileOptions} from 'child_process';
 import {execFile as execFileAsync} from 'child_process';
 import {readFile} from 'fs/promises';
 import {join} from 'path';
@@ -11,7 +12,22 @@ import {promisify} from 'util';
 
 import {ASSETS_DIR} from './constants.js';
 
-export const execFile = promisify(execFileAsync);
+const nodeExecFile = promisify(execFileAsync);
+
+export const execFile = async (
+  file: string,
+  args: string[],
+  options: ExecFileOptions
+): Promise<{
+  stdout: string;
+  stderr: string;
+}> => {
+  console.log(file, args);
+  const result = await nodeExecFile(file, args, options);
+  console.log('stdout', result.stdout);
+  console.log('stderr', result.stderr);
+  return result;
+};
 export const readAsset = (...components: string[]): Promise<string> => {
   return readFile(join(ASSETS_DIR, ...components), 'utf8');
 };
