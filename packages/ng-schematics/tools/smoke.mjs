2e3af0cc81aa9e8ce3e0b6b5f760c70c3c09aa5e
Nikolay Vitkov <nvitkov@chromium.org>
2024-07-05 14:24:40 +0200

    chore: verify installed

--- packages/ng-schematics/tools/smoke.mjs
+++ packages/ng-schematics/tools/smoke.mjs
@@ -25,10 +25,28 @@ const {values: args} = parseArgs({
   },
 });
 
-if (process.env.CI) {
-  // Need to install in CI
-  execSync('npm install -g @angular/cli@latest @angular-devkit/schematics-cli');
+function verifyAngularCliInstalled() {
+  if (process.env.CI) {
+    // Need to install in CI
+    execSync(
+      'npm install -g @angular/cli@latest @angular-devkit/schematics-cli'
+    );
+    return;
+  }
+  const userDeps = execSync('npm list -g --depth=0');
+
+  if (
+    !userDeps.includes('@angular/cli') ||
+    !userDeps.includes('@angular-devkit/schematics-cli')
+  ) {
+    console.error(
+      'Angular CLI not installed run `npm install -g @angular/cli @angular-devkit/schematics-cli`'
+    );
+    process.exit(1);
+  }
 }
+
+verifyAngularCliInstalled();
 
 if (!args.runner) {
   const runners = ['node', 'jest', 'jasmine', 'mocha'];
