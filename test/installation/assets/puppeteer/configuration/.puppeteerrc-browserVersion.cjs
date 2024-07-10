bca750afe204cc3bafb0a34a0f92b0bac5a6a55f
Alex Rudenko <OrKoN@users.noreply.github.com>
2024-07-10 13:23:46 +0200

    fix(cli): puppeteer CLI should read the project configuration (#12730)

---
+++ test/installation/assets/puppeteer/configuration/.puppeteerrc-browserVersion.cjs
@@ -0,0 +1,9 @@
+const {join} = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  browserRevision: '121',
+};
--- test/installation/src/puppeteer-configuration.spec.ts
+++ test/installation/src/puppeteer-configuration.spec.ts
@@ -5,6 +5,8 @@
  */
 
 import assert from 'assert';
+import {spawnSync} from 'child_process';
+import {existsSync, readFileSync} from 'fs';
 import {readdir, writeFile} from 'fs/promises';
 import {join} from 'path';
 
@@ -68,6 +70,61 @@ describe('`puppeteer` with configuration', () => {
 
       const script = await readAsset('puppeteer', 'basic.js');
       await this.runScript(script, 'mjs');
+    });
+  });
+
+  describe('CLI', () => {
+    configureSandbox({
+      dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
+      env: cwd => {
+        return {
+          PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
+          PUPPETEER_SKIP_DOWNLOAD: 'true',
+        };
+      },
+      before: async cwd => {
+        await writeFile(
+          join(cwd, '.puppeteerrc.cjs'),
+          // config specifies 121 as the browserVersion.
+          await readAsset(
+            'puppeteer',
+            'configuration',
+            '.puppeteerrc-browserVersion.cjs'
+          )
+        );
+      },
+    });
+
+    it('installs the browser version from the configuration', async function () {
+      assert.ok(!existsSync(join(this.sandbox, '.cache', 'puppeteer')));
+      const result = spawnSync(
+        'npx',
+        ['puppeteer', 'browsers', 'install', 'chrome'],
+        {
+          // npx is not found without the shell flag on Windows.
+          shell: process.platform === 'win32',
+          cwd: this.sandbox,
+          env: {
+            ...process.env,
+            PUPPETEER_CACHE_DIR: join(this.sandbox, '.cache', 'puppeteer'),
+          },
+        }
+      );
+      assert.strictEqual(
+        result.status,
+        0,
+        `${result.stdout}\n${result.stderr}`
+      );
+      const metadataFilePath = join(
+        this.sandbox,
+        '.cache',
+        'puppeteer',
+        'chrome',
+        '.metadata'
+      );
+      assert.ok(existsSync(metadataFilePath));
+      const metadata = JSON.parse(readFileSync(metadataFilePath, 'utf8'));
+      assert.ok(metadata['aliases']['121']);
     });
   });
 });
