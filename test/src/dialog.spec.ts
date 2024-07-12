5c8a1675c5cb8f8199c5895d2cfacfa1519730d4
Nikolay Vitkov <nvitkov@chromium.org>
2024-07-12 14:53:51 +0200

    chore(build): update chromium-bidi to 0.6.1

--- test/src/dialog.spec.ts
+++ test/src/dialog.spec.ts
@@ -11,7 +11,7 @@ import {getTestState, setupTestBrowserHooks} from './mocha-utils.js';
 describe('Page.Events.Dialog', function () {
   setupTestBrowserHooks();
 
-  it('should fire', async () => {
+  it.only('should fire', async () => {
     const {page} = await getTestState();
 
     const onDialog = sinon.stub().callsFake(dialog => {
