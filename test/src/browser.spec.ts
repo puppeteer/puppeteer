e171d087183e8c44f494bcb554f01836b9c5f9ce
Nikolay Vitkov <nvitkov@chromium.org>
2024-07-16 16:59:16 +0200

    chore: fix

--- test/src/browser.spec.ts
+++ test/src/browser.spec.ts
@@ -21,7 +21,7 @@ describe('Browser specs', function () {
     });
   });
 
-  describe.only('Browser.userAgent', function () {
+  describe('Browser.userAgent', function () {
     it('should include Browser engine', async () => {
       const {browser, isChrome} = await getTestState();
 
@@ -29,9 +29,18 @@ describe('Browser specs', function () {
       expect(userAgent.length).toBeGreaterThan(0);
       if (isChrome) {
         expect(userAgent).toContain('WebKit');
-        expect(userAgent).toContain('Chrome');
       } else {
         expect(userAgent).toContain('Gecko');
+      }
+    });
+    it('should include Browser name', async () => {
+      const {browser, isChrome} = await getTestState();
+
+      const userAgent = await browser.userAgent();
+      expect(userAgent.length).toBeGreaterThan(0);
+      if (isChrome) {
+        expect(userAgent).toContain('Chrome');
+      } else {
         expect(userAgent).toContain('Firefox');
       }
     });
