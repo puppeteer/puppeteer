2219c2de6e9fe3a22810cdc11a6e84940699589a
Alex Rudenko <alexrudenko@chromium.org>
2024-07-24 06:39:37 +0200

    feat: roll to Chrome 127.0.6533.72

--- packages/puppeteer-core/src/revisions.ts
+++ packages/puppeteer-core/src/revisions.ts
@@ -8,7 +8,7 @@
  * @internal
  */
 export const PUPPETEER_REVISIONS = Object.freeze({
-  chrome: '126.0.6478.182',
-  'chrome-headless-shell': '126.0.6478.182',
+  chrome: '127.0.6533.72',
+  'chrome-headless-shell': '127.0.6533.72',
   firefox: 'latest',
 });
