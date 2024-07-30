554e2ffa128ad0ef9715b28dfed40d7335ff5e14
camfung <cameronfuung@gmail.com>
2024-07-30 00:49:48 -0700

    fixed typo

--- packages/puppeteer-core/src/api/Page.ts
+++ packages/puppeteer-core/src/api/Page.ts
@@ -2674,7 +2674,7 @@ export abstract class Page extends EventEmitter<PageEvents> {
    * Shortcut for {@link Frame.click | page.mainFrame().click(selector[, options]) }.
    * @param selector - A
    * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors | selector}
-   * of an element to type into. If there are multiple elements satisfying the
+   * of an element to click. If there are multiple elements satisfying the
    * selector, the first will be clicked.
    * @param options - `Object`
    * @returns Promise which resolves when the element matching `selector` is
