84c9932273b682aa9cf6288573ec0ce8f9a4cfaf
Nikolay Vitkov <nvitkov@chromium.org>
2024-08-08 17:43:37 +0200

    chore: prevent running clean from outside npm

--- tools/clean.mjs
+++ tools/clean.mjs
@@ -8,11 +8,49 @@
 
 import {exec} from 'child_process';
 import {readdirSync} from 'fs';
+import readline from 'node:readline';
 
-exec(
-  `git clean -Xf ${readdirSync(process.cwd())
-    .filter(file => {
-      return file !== 'node_modules';
-    })
-    .join(' ')}`
-);
+let resolver;
+let rejector;
+const confirmation = new Promise((res, rej) => {
+  resolver = res;
+  rejector = rej;
+});
+
+if (process.env.npm_command !== 'run-script') {
+  console.log(
+    'Running command outside from non Puppeteer directory may result in data loss'
+  );
+  console.log('Ref: https://github.com/puppeteer/puppeteer/issues/12917');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Do you want to proceed? (y/N) ', answer => {
+    rl.close();
+    if (
+      answer.localeCompare('y', undefined, {
+        sensitivity: 'base',
+      }) === 'y' ||
+      answer === 'yes'
+    ) {
+      resolver();
+    } else {
+      rejector('Canceled');
+    }
+  });
+} else {
+  resolver();
+}
+
+try {
+  await confirmation;
+  exec(
+    `git clean -Xf ${readdirSync(process.cwd())
+      .filter(file => {
+        return file !== 'node_modules';
+      })
+      .join(' ')}`
+  );
+} catch {}
