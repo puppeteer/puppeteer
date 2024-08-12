11ea0364618ebef8793c1bc05b94b90a01014b0f
Nikolay Vitkov <34244704+Lightning00Blade@users.noreply.github.com>
2024-08-12 16:22:16 +0200

    test: exit installation tests if on CI (#12944)

--- test/installation/.mocharc.cjs
+++ test/installation/.mocharc.cjs
@@ -9,5 +9,12 @@
  */
 module.exports = {
   spec: ['build/**/*.spec.js'],
-  timeout: '240000ms',
+  require: ['source-map-support/register'],
+  exit: !!process.env.CI,
+  timeout: 240_000,
+  // This should make mocha crash on uncaught errors.
+  // See https://github.com/mochajs/mocha/blob/master/docs/index.md#--allow-uncaught.
+  allowUncaught: true,
+  // See https://github.com/mochajs/mocha/blob/master/docs/index.md#--async-only--a.
+  asyncOnly: true,
 };
