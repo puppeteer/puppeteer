2cfbcaefe20af3ad207fcf67fdb58854d58efd5f
Alex Rudenko <alexrudenko@chromium.org>
2024-09-25 10:02:19 +0200

    build: add a es5-iife configuration

---
+++ packages/puppeteer-core/rollup.config.mjs
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {getBabelOutputPlugin} from '@rollup/plugin-babel';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'lib/esm/puppeteer/puppeteer-core-browser.js',
+  output: {
+    format: 'iife',
+    file: 'lib/es5-iife/puppeteer-core-browser.js',
+    name: 'Puppeteer',
+  },
+  external: (id, parent, isResolved) => {
+    if (id.includes('BrowserConnector')) {
+      return false;
+    }
+    if (id.includes('BrowserWebSocketTransport')) {
+      return false;
+    }
+    if (id.includes('puppeteer/node')) {
+      return true;
+    }
+    if (id.includes('chromium-bidi') || id.includes('puppeteer/bidi')) {
+      return true;
+    }
+    return false;
+  },
+  plugins: [
+    nodeResolve({
+      browser: true,
+    }),
+    getBabelOutputPlugin({
+      plugins: [
+        '@babel/plugin-transform-class-static-block',
+        '@babel/plugin-transform-private-methods',
+        '@babel/plugin-transform-private-property-in-object',
+        '@babel/plugin-transform-class-properties',
+        '@babel/plugin-transform-dynamic-import',
+        '@babel/plugin-transform-modules-commonjs',
+      ],
+      allowAllFormats: true,
+      compact: false,
+    }),
+  ],
+};
---
+++ tools/patch.mjs
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import fs from 'fs';
+
+const genJs = process.argv[2];
+const genTypes = process.argv[3];
+const inputTypes = process.argv[4];
+
+let file = fs.readFileSync(genJs, 'utf-8');
+
+file = file.replaceAll('__proto__: [],', '');
+file = file.replaceAll('__proto__: null,', '');
+file = file.replaceAll('__proto__: [],', '');
+
+file = file.replaceAll('__proto__: []', '');
+file = file.replaceAll('__proto__: null', '');
+file = file.replaceAll('d2.__proto__ = b2;', '');
+file = file.replaceAll('__proto__: []', '');
+
+file = file.replaceAll('fn = new Function(`return ${functionValue}`)();', '');
+file = file.replaceAll('new Function(`(${value})`);', '// stripped');
+file = file.replaceAll(
+  `return await this.evaluate(html => {
+          document.open();
+          document.write(html);
+          document.close();
+        }, content);`,
+  'throw new Error("unsupported")'
+);
+file = file.replaceAll('link.href = url;', 'throw new Error("unsupported")');
+file = file.replaceAll('script.src = url;', 'throw new Error("unsupported")');
+
+fs.writeFileSync(genJs, file);
+
+fs.copyFileSync(inputTypes, genTypes);
+
+file = fs.readFileSync(genTypes, 'utf-8');
+
+file = file.replaceAll('#private;', '');
+
+file = file.replaceAll(
+  `import { Session } from 'chromium-bidi/lib/cjs/protocol/protocol.js';`,
+  'type Session = any;'
+);
+file = file.replaceAll(
+  `import type { ParseSelector } from 'typed-query-selector/parser.js';`,
+  'type ParseSelector<T extends string> = any;'
+);
+
+file = file.replaceAll(
+  `export declare type SupportedWebDriverCapability = Exclude<Session.CapabilityRequest, 'unhandledPromptBehavior' | 'acceptInsecureCerts'>;`,
+  'export declare type SupportedWebDriverCapability = any;'
+);
+
+fs.writeFileSync(genTypes, file);
