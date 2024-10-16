496dfcbbdc4c7cf4269cd441d63b1e45232ef84d
Nikolay Vitkov <nvitkov@chromium.org>
2024-10-16 18:05:28 +0200

    chore: random new errors

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -8,5 +8,10 @@
  */
 module.exports = {
   ...require('gts/.prettierrc.json'),
-  // proseWrap: 'always', // Uncomment this while working on Markdown documents. MAKE SURE TO COMMENT THIS BEFORE RUNNING CHECKS/FORMATS OR EVERYTHING WILL BE MODIFIED.
+  /**
+   * Uncomment this while working on Markdown documents.
+   * MAKE SURE TO COMMENT THIS BEFORE RUNNING CHECKS/FORMATS
+   * OR EVERYTHING WILL BE MODIFIED.
+   */
+  // proseWrap: 'always',
 };
