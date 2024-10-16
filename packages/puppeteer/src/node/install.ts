fc968b6304ad40a2e5406304739dc43742e435cf
Nikolay Vitkov <nvitkov@chromium.org>
2024-10-16 18:05:13 +0200

    chore: eslint auto-removed comments

--- packages/puppeteer/src/node/install.ts
+++ packages/puppeteer/src/node/install.ts
@@ -140,7 +140,6 @@ function logPolitely(toBeLogged: unknown): void {
   const logLevel = process.env['npm_config_loglevel'] || '';
   const logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 
-  // eslint-disable-next-line no-console
   if (!logLevelDisplay) {
     console.log(toBeLogged);
   }
