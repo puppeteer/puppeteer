20f9f15d5f94832bd6f5c0e9807a1a53182c49f8
Alex Rudenko <OrKoN@users.noreply.github.com>
2024-12-19 16:15:53 +0100

    refactor!: remove deprecated Launch and Connect options (#13426)

--- packages/puppeteer-core/src/node/LaunchOptions.ts
+++ packages/puppeteer-core/src/node/LaunchOptions.ts
@@ -133,21 +133,3 @@ export interface LaunchOptions extends ConnectOptions {
    */
   args?: string[];
 }
-
-/**
- * @deprecated Use {@link LaunchOptions}.
- * @public
- */
-export type BrowserLaunchArgumentOptions = LaunchOptions;
-
-/**
- * @deprecated Use {@link LaunchOptions}.
- * @public
- */
-export type PuppeteerNodeLaunchOptions = LaunchOptions;
-
-/**
- * @deprecated Use {@link LaunchOptions}.
- * @public
- */
-export type PuppeteerLaunchOptions = LaunchOptions;
