d98c877ead8cf2f32fe793260d2b2c2f618964f9
Alex Rudenko <OrKoN@users.noreply.github.com>
2025-01-14 11:44:58 +0100

    docs: document the channel parameter better (#13497)

--- packages/puppeteer-core/src/node/LaunchOptions.ts
+++ packages/puppeteer-core/src/node/LaunchOptions.ts
@@ -22,7 +22,8 @@ export type ChromeReleaseChannel =
  */
 export interface LaunchOptions extends ConnectOptions {
   /**
-   * Chrome Release Channel
+   * If specified for Chrome, looks for a regular Chrome installation at a known
+   * system location instead of using the bundled Chrome binary.
    */
   channel?: ChromeReleaseChannel;
   /**
