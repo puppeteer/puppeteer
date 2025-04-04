6aa964cabf15ece0cb664294d8a64f290d7380cc
madhuradivekar <madhura.divekar@infosys.com>
2025-04-04 11:34:15 +0200

    docs: replace Usage of deprecated "name" method in Frame documentation (#13703)
    Co-authored-by: Nikolay Vitkov <nvitkov@chromium.org>

--- packages/puppeteer-core/src/api/Frame.ts
+++ packages/puppeteer-core/src/api/Frame.ts
@@ -218,7 +218,7 @@ export const throwIfDetached = throwIfDisposed<Frame>(frame => {
  *
  * To understand frames, you can think of frames as `<iframe>` elements. Just
  * like iframes, frames can be nested, and when JavaScript is executed in a
- * frame, the JavaScript does not effect frames inside the ambient frame the
+ * frame, the JavaScript does not affect frames inside the ambient frame the
  * JavaScript executes in.
  *
  * @example
@@ -251,9 +251,25 @@ export const throwIfDetached = throwIfDisposed<Frame>(frame => {
  * An example of getting text from an iframe element:
  *
  * ```ts
- * const frame = page.frames().find(frame => frame.name() === 'myframe');
- * const text = await frame.$eval('.selector', element => element.textContent);
- * console.log(text);
+ * const frames = page.frames();
+ * let frame = null;
+ * for (const currentFrame of frames) {
+ *   const frameElement = await currentFrame.frameElement();
+ *   const name = await frameElement.evaluate(el => el.getAttribute('name'));
+ *   if (name === 'myframe') {
+ *     frame = currentFrame;
+ *     break;
+ *   }
+ * }
+ * if (frame) {
+ *   const text = await frame.$eval(
+ *     '.selector',
+ *     element => element.textContent,
+ *   );
+ *   console.log(text);
+ * } else {
+ *   console.error('Frame with name "myframe" not found.');
+ * }
  * ```
  *
  * @remarks
