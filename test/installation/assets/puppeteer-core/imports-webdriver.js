0aa45e0b381f5461feb587b633b9b2e5d3272e5d
Alex Rudenko <alexrudenko@chromium.org>
2025-05-06 10:41:42 +0200

    fix(webdriver): bump chromium-bidi

---
+++ test/installation/assets/puppeteer-core/imports-webdriver.js
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2025 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import 'puppeteer-core';
+import 'puppeteer-core/internal/revisions.js';
+import 'puppeteer-core/lib/esm/puppeteer/revisions.js';
+import 'puppeteer-core/lib/esm/puppeteer/bidi/BidiOverCdp.js';
