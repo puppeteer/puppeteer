3db51d40ca40dd100c79aa21d2d3af20a060aa86
Alex Rudenko <alexrudenko@chromium.org>
2025-09-18 12:39:23 +0200

    test: update eval test for RenderDocument

--- test/src/evaluation.spec.ts
+++ test/src/evaluation.spec.ts
@@ -146,7 +146,10 @@ describe('Evaluation specs', function () {
         .catch(error_ => {
           return (error = error_);
         });
-      expect(error.message).toContain('Protocol error');
+      expect(error.message).atLeastOneToContain([
+        'Execution context was destroyed', // Chrome
+        'no such frame', // Firefox
+      ]);
     });
     it('should await promise', async () => {
       const {page} = await getTestState();
