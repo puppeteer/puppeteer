d0e95b19796d6492d09244eb2fe7918a23b2d65d
Alex Rudenko <OrKoN@users.noreply.github.com>
2025-09-23 11:45:52 +0200

    fix: replace function eval with parsing in page.evaluate() (#14226)

--- packages/puppeteer-core/src/util/Function.test.ts
+++ packages/puppeteer-core/src/util/Function.test.ts
@@ -8,7 +8,7 @@ import {describe, it} from 'node:test';
 
 import expect from 'expect';
 
-import {interpolateFunction} from './Function.js';
+import {interpolateFunction, stringifyFunction} from './Function.js';
 
 describe('Function', function () {
   describe('interpolateFunction', function () {
@@ -31,6 +31,62 @@ describe('Function', function () {
         {test: `() => 5`},
       );
       expect(test()).toBe(5);
+    });
+  });
+
+  describe('stringifyFunction', () => {
+    // prettier-ignore
+    it('should work', () => { /* eslint-disable @typescript-eslint/no-unused-expressions */
+      class K1 { m() {} }
+      class K2 { m () {} }
+      class K3 { async m() {} }
+      class K4 { async m () {} }
+
+      const variations = [
+        // Non-arrow functions.
+        function() {},
+        function () {},
+        function x() {},
+        function x () {},
+        async function() {},
+        async function () {},
+        async function x() {},
+        async function x () {},
+        // Concise methods.
+        new K1().m,
+        new K2().m,
+        new K3().m,
+        new K4().m,
+        // Arrow functions.
+        () => {},
+        // @ts-expect-error irrelevant
+        (a) => {a;},
+        // @ts-expect-error irrelevant
+        a => {a;},
+        async () => {},
+        // @ts-expect-error irrelevant
+        async (a) => {a;},
+        // @ts-expect-error irrelevant
+        async a => {a;},
+        // Generator functions
+        function*a() {},
+        function *a() {},
+        function* a() {},
+        // Async generator functions
+        async function*a() {},
+        async function *a() {},
+        async function* a() {},
+      ];
+
+      for (const v of variations) {
+        const fnStr = stringifyFunction(v);
+        try {
+          new Function(`(${fnStr})`);
+        } catch {
+          throw new Error(`Could not stringify:
+${v.toString()}`);
+        }
+      }
     });
   });
 });
