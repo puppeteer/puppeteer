9d7d26b1d0d0e4a74ba4200e67fbd7d79ad5c5ba
Nikolay Vitkov <nvitkov@chromium.org>
2025-10-22 00:32:55 +0200

    fix windows

--- test/fixtures/closeme.mjs
+++ test/fixtures/closeme.mjs
@@ -3,7 +3,9 @@
  * Copyright 2024 Google Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
+import {pathToFileURL} from 'node:url';
+
 const [, , puppeteerRoot, options] = process.argv;
-const puppeteer = (await import(puppeteerRoot)).default;
+const puppeteer = (await import(pathToFileURL(puppeteerRoot))).default;
 const browser = await puppeteer.launch(JSON.parse(options));
 console.log(browser.wsEndpoint());
--- test/fixtures/dumpio.mjs
+++ test/fixtures/dumpio.mjs
@@ -3,8 +3,10 @@
  * Copyright 2024 Google Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
+import {pathToFileURL} from 'node:url';
+
 const [, , puppeteerRoot, options] = process.argv;
-const puppeteer = (await import(puppeteerRoot)).default;
+const puppeteer = (await import(pathToFileURL(puppeteerRoot))).default;
 const browser = await puppeteer.launch(JSON.parse(options));
 const page = await browser.newPage();
 await page.evaluate(() => {
