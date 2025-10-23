fdd4dd3fd0e799dd8c702fb11d6d262dfd637806
Nikolay Vitkov <nvitkov@chromium.org>
2025-10-23 18:42:06 +0200

    fixes

--- test/installation/src/puppeteer-typescript.spec.ts
+++ test/installation/src/puppeteer-typescript.spec.ts
@@ -12,12 +12,12 @@ import {configureSandbox} from './sandbox.js';
 import {execFile, readAsset} from './util.js';
 
 // Skipping this test on Windows as windows runners are much slower.
-(platform() === 'win32' ? describe.skip : describe)(
+(platform() === 'win32' ? describe.skip : describe.only)(
   '`puppeteer` with TypeScript',
   () => {
     configureSandbox({
       dependencies: ['@puppeteer/browsers', 'puppeteer-core', 'puppeteer'],
-      devDependencies: ['typescript@4.7.4', '@types/node@18.17.15'],
+      devDependencies: ['typescript@5.0.2', '@types/node@20.19.23'],
       env: cwd => {
         return {
           PUPPETEER_CACHE_DIR: join(cwd, '.cache', 'puppeteer'),
@@ -25,7 +25,27 @@ import {execFile, readAsset} from './util.js';
       },
     });
 
-    it('should work', async function () {
+    it('should work with ESM', async function () {
+      // Write a Webpack configuration.
+      await writeFile(
+        join(this.sandbox, 'tsconfig.json'),
+        await readAsset('puppeteer', 'tsconfig.json'),
+      );
+
+      // Write the source code.
+      await writeFile(
+        join(this.sandbox, 'index.mts'),
+        await readAsset('puppeteer', 'basic.ts'),
+      );
+
+      // Compile.
+      await execFile('npx', ['tsc'], {cwd: this.sandbox, shell: true});
+
+      const script = await readFile(join(this.sandbox, 'index.mjs'), 'utf-8');
+
+      await this.runScript(script, 'mjs');
+    });
+    it('should work with CJS', async function () {
       // Write a Webpack configuration.
       await writeFile(
         join(this.sandbox, 'tsconfig.json'),
@@ -35,7 +55,7 @@ import {execFile, readAsset} from './util.js';
       // Write the source code.
       await writeFile(
         join(this.sandbox, 'index.ts'),
-        await readAsset('puppeteer', 'basic.ts'),
+        await readAsset('puppeteer', 'basic-cjs.ts'),
       );
 
       // Compile.
