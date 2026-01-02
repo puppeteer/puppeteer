79c7a62d68ce742fdc70972378ef5666bd31e8b1
Nicholas Roscino <nroscino@google.com>
2026-01-02 13:53:43 +0000

    chore(puppeteer-core): Addressing PR comments

--- packages/puppeteer-core/src/api/HTTPResponse.test.ts
+++ packages/puppeteer-core/src/api/HTTPResponse.test.ts
@@ -8,28 +8,33 @@ import {describe, it} from 'node:test';
 
 import {HTTPResponse} from './HTTPResponse.js';
 
-//@ts-ignore
+//@ts-expect-error we don't need to implement all the methods for the tests on
+// text method
 class TestResponse extends HTTPResponse {
-  constructor(private readonly contentToReturn: Uint8Array) { super(); }
+  private _contentToReturn: Uint8Array = new Uint8Array();
+
+  set contentToReturn(contentToReturn: Uint8Array) {
+    this._contentToReturn = contentToReturn;
+  }
 
   override content(): Promise<Uint8Array> {
-    return Promise.resolve(this.contentToReturn);
+    return Promise.resolve(this._contentToReturn);
   }
 }
 
 describe('HTTPResponse', () => {
   describe('text', () => {
     it('should return the content as text', async () => {
-      const content = new TextEncoder().encode('hello');
-      const testResponse = new TestResponse(content);
+      const testResponse = new TestResponse();
+      testResponse.contentToReturn = new TextEncoder().encode('hello');
 
       const text = await testResponse.text();
       expect(text).toBe('hello');
     });
 
     it('should throw if content is not valid UTF-8', async () => {
-      const content = new Uint8Array([ 0xff ]);
-      const testResponse = new TestResponse(content);
+      const testResponse = new TestResponse();
+      testResponse.contentToReturn = new Uint8Array([ 0xff ]);
 
       await expect(testResponse.text()).rejects.toThrow();
     });
