a994a65840dbf69a5b1a658c8938a615f69a8894
Henrik Skupin <mail@hskupin.info>
2026-01-30 10:04:52 +0100

    test: check previously skipped tests for Firefox

--- test/src/page.spec.ts
+++ test/src/page.spec.ts
@@ -424,7 +424,7 @@ describe('Page', function () {
   });
 
   describe('Page.setGeolocation', function () {
-    it('should work', async () => {
+    it.only('should work', async () => {
       const {page, server, context} = await getTestState();
 
       await context.overridePermissions(server.PREFIX, ['geolocation']);
