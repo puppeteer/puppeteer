a4662740594955a6ec611c49a1e7650d17699998
Piotr Paulski <piotrpaulski@chromium.org>
2026-03-05 12:08:44 +0000

    Update chrome version to include crrev/7615848

--- packages/puppeteer-core/src/revisions.ts
+++ packages/puppeteer-core/src/revisions.ts
@@ -8,7 +8,7 @@
  * @internal
  */
 export const PUPPETEER_REVISIONS = Object.freeze({
-  chrome: '146.0.7680.31',
-  'chrome-headless-shell': '146.0.7680.31',
+  chrome: '147.0.7713.0',
+  'chrome-headless-shell': '147.0.7713.0',
   firefox: 'stable_148.0',
 });
