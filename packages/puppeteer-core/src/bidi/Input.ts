a35025f73a4e554ac920747082b7b26d5efd513b
Henrik Skupin <mail@hskupin.info>
2026-03-16 21:23:31 +0100

    fix: without azimuthAngle the altitudeAngle should no be specified

--- packages/puppeteer-core/src/bidi/Input.ts
+++ packages/puppeteer-core/src/bidi/Input.ts
@@ -732,7 +732,6 @@ export class BidiTouchscreen extends Touchscreen {
       width: 0.5 * 2, // 2 times default touch radius.
       height: 0.5 * 2, // 2 times default touch radius.
       pressure: 0.5,
-      altitudeAngle: Math.PI / 2,
     };
     const touch = new BidiTouchHandle(this.#page, this, id, x, y, properties);
     await touch.start(options);
