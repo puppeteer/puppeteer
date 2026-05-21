94d1e1c3dc84ed102d2e801a4e6343cd9fd4a9af
Alex Rudenko <OrKoN@users.noreply.github.com>
2026-05-21 11:55:00 +0200

    refactor: remove debug dependency (#15023)

--- packages/browsers/.mocharc.cjs
+++ packages/browsers/.mocharc.cjs
@@ -3,10 +3,12 @@
  * Copyright 2024 Google Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
+process.env.NODE_DEBUG = 'puppeteer:*';
+
 module.exports = {
   logLevel: 'debug',
   spec: 'test/build/**/*.spec.js',
-  require: ['./test/build/mocha-utils.js', 'source-map-support/register'],
+  require: ['source-map-support/register'],
   exit: !!process.env.CI,
   reporter: 'spec',
   timeout: 10_000,
