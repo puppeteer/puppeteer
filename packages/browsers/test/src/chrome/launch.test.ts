3a33d3d87337dcc02d9c642c3313dd4c15e1f559
Nikolay Vitkov <nvitkov@chromium.org>
2026-07-28 12:17:23 +0000

    fix

--- packages/browsers/test/src/chrome/launch.test.ts
+++ packages/browsers/test/src/chrome/launch.test.ts
@@ -187,28 +187,21 @@ describe('Chrome', () => {
         browser: Browser.CHROME,
         buildId: testChromeBuildId,
       });
-      const profileDir = path.join(tmpDir, 'profile');
       let hookRan = false;
       const browserProcess = launch({
         executablePath,
         args: getArgs(),
         onExit: async () => {
           hookRan = true;
-          if (fs.existsSync(profileDir)) {
-            fs.rmSync(profileDir, {recursive: true, force: true});
-          }
         },
       });
       await browserProcess.waitForLineOutput(CDP_WEBSOCKET_ENDPOINT_REGEX);
 
-      // Simulate the Node.js host process receiving SIGINT
-      process.emit('SIGINT');
+      process.emit('SIGINT', 'SIGINT');
 
-      // Wait for it to close!
       await browserProcess.hasClosed();
 
       assert.ok(hookRan);
-      assert.ok(!fs.existsSync(profileDir));
       assert.strictEqual(process.exitCode, 130);
     });
   });
