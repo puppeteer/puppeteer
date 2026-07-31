012631c693191c072a023c578d00eb4e683ea389
Wolfgang Beyer <wolfi@chromium.org>
2026-07-31 12:50:41 +0000

    disable WebUIOmniboxPopup

--- packages/puppeteer-core/src/node/ChromeLauncher.ts
+++ packages/puppeteer-core/src/node/ChromeLauncher.ts
@@ -201,6 +201,8 @@ export class ChromeLauncher extends BrowserLauncher {
       'MediaRouter',
       'OptimizationHints',
       'WebUIReloadButton',
+      'WebUIOmniboxPopup',
+      'WebUIOmniboxAimPopup',
       ...(turnOnExperimentalFeaturesForTesting
         ? []
         : [
