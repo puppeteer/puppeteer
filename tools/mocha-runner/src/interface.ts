5f1c69dc4f36f5f442c0e4b631f6c70e5f8c41c8
Nikolay Vitkov <nvitkov@chromium.org>
2026-09-13 18:43:46 +0000

    fix

--- tools/mocha-runner/src/interface.ts
+++ tools/mocha-runner/src/interface.ts
@@ -4,10 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * This file needs to be kept CJS until Mocha starts accepting ESM
- * for custom interfaces
- */
 
 import Mocha from 'mocha';
 // @ts-expect-error
--- tools/mocha-runner/src/reporter.ts
+++ tools/mocha-runner/src/reporter.ts
@@ -4,10 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * This file needs to be kept CJS until Mocha starts accepting ESM
- * for custom reporters
- */
 
 import Mocha from 'mocha';
 
